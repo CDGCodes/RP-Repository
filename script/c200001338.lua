d670aa769b65f5c85f863ca1a60bf132ab0d0c50
Dylan <dpunter8562@gmail.com>
2024-03-06 12:26:31 -0800

    Added missing functions for summon condition

--- script/c200001338.lua
+++ script/c200001338.lua
@@ -1,8 +1,14 @@
- --Black Rose Magician Girl
- local s,id=GetID()
- function s.initial_effect(c)
- 	--contact fusion
- 	c:EnableReviveLimit()
- 	Fusion.AddProcMix(c,true,true,38033121,73580471)
- 	Fusion.AddContactProc(c,s.contactfil,s.contactop,s.splimit,nil,nil,nil,false)
- end
+--Black Rose Magician Girl
+local s,id=GetID()
+function s.initial_effect(c)
+	--contact fusion
+	c:EnableReviveLimit()
+	Fusion.AddProcMix(c,true,true,38033121,73580471)
+	Fusion.AddContactProc(c,s.contactfil,s.contactop,s.splimit,nil,nil,nil,false)
+end
+function s.contactfil(tp)
+	return Duel.GetMatchingGroup(Card.IsAbleToGraveAsCost,tp,LOCATION_MZONE,0,nil)
+end
+function s.contactop(g)
+	Duel.SendtoGrave(g,REASON_COST+REASON_MATERIAL)
+end
