94bc7f0e7cb9253d06c30285d134e72d7287224f
Dylan <dpunter8562@gmail.com>
2024-03-06 14:25:51 -0800

    Added attack adjustment effect to Black Rose Magician Girl

--- script/c200001338.lua
+++ script/c200001338.lua
@@ -37,7 +37,7 @@ function s.splimit(e,se,sp,st)
 end
 
 function s.val(e, c)
-	return Duel.GetMatchingGroupCount(Card.IsMonster,e:GetHandlerPlayer(),LOCATION_GRAVE,0,nil)*200
+	return Duel.GetMatchingGroupCount(Card.IsMonster,e:GetHandlerPlayer(),LOCATION_MZONE,LOCATION_MZONE,nil, 0x20a2)*200
 end
 
 function s.cfilter(c, tp)
