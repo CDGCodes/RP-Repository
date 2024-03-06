d684b1776b28a7d425c3cd4af7e677aefe60d643
Dylan <dpunter8562@gmail.com>
2024-03-06 12:18:05 -0800

    Added summon condition to Black Rose Magician Girl

--- script/c200001338.lua
+++ script/c200001338.lua
@@ -3,6 +3,6 @@
  function s.initial_effect(c)
  	--contact fusion
  	c:EnableReviveLimit()
- 	Fusion.AddProcMix(c,true,true,38033121,73580471)
+ 	Fusion.AddProcMixN(c,true,true,38033121,73580471)
  	Fusion.AddContactProc(c,s.contactfil,c.contactop,s.splimit,nil,nil,nil,false)
  end
