2f28e093480c1f0c9fc5cf18ab69ebb1257bf6fd
Dylan <dpunter8562@gmail.com>
2024-03-06 21:06:44 -0800

    Added test effect

--- script/c200001339.lua
+++ script/c200001339.lua
@@ -8,15 +8,27 @@ function s.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
 	--e1:SetCategory(CATEGORY_HANDES+CATEGORY_DAMAGE)
 	--e1:SetType(EFFECT_TYPE_FIELD)
-	--e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetCountLimit(1)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCost(aux.dxmcostgen(1,1,nil))
 	--e1:SetCondition(s.condition)
 	--e1:SetTarget(s.target)
 	--e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
+	--multi destroy
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,0))
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetCategory(CATEGORY_DESTROY)
+	e3:SetCountLimit(1)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCondition(s.mtcon)
+	e3:SetOperation(s.mtop)
+	c:RegisterEffect(e3)
 end
+
 s.xyz_number=108
+
 function s.condition(e, tp, eg, ep, ev, re, r, rp)
 	return true
 end
@@ -32,4 +44,32 @@ function s.activate(e, tp, eg, ep, ev, re, r, rp)
 	local tc=g:RandomSelect(tp, 1, 1, nil)
 	Duel.BreakEffect()
 	Duel.SendtoGrave(tc, REASON_EFFECT)
+end
+
+function s.mtcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)>=5
+end
+function s.mtop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	Duel.ConfirmDecktop(tp,5)
+	local g=Duel.GetDecktopGroup(tp,5)
+	local tt=g:FilterCount(Card.IsType,nil,TYPE_TUNER)
+	local gt=g:FilterCount(Card.IsType,nil,TYPE_MONSTER)
+	local ct=gt-tt
+	Duel.ShuffleDeck(tp)
+	if ct>0 then
+		Duel.BreakEffect()
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+		local f=Duel.GetMatchingGroup(aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,nil)
+		local sg=f:Select(tp,ct,math.min(ct,#g),nil)
+		local dmg = Duel.Destroy(sg,REASON_EFFECT)
+		if dmg>0 then
+			local e1=Effect.CreateEffect(c)
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_UPDATE_ATTACK)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE+RESET_PHASE+PHASE_END)
+			e1:SetValue(dmg*500)
+			c:RegisterEffect(e1)
+		end
+	end
 end
