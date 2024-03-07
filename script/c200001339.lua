5b9aa588fd3d74cf28d65eb076f591886c5aecc1
Dylan <dpunter8562@gmail.com>
2024-03-06 20:48:19 -0800

    Added discard effect

--- script/c200001339.lua
+++ script/c200001339.lua
@@ -18,7 +18,8 @@ function s.initial_effect(c)
 end
 s.xyz_number=108
 function s.condition(e, tp, eg, ep, ev, re, r, rp)
-	return (Duel.GetFieldGroupCount(tp, 0, LOCATION_HAND) > 0)
+	if Duel.GetFieldGroupCount(tp, 0, LOCATION_HAND) > 0 then return false end
+	return true
 end
 function s.target(e, tp, eg, ep, ev, re, r, rp, chk)
 	if chk==0 then return false end
