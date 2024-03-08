2ce41557b53a062700df34da5721c89b57239736
Dylan <dpunter8562@gmail.com>
2024-03-08 15:40:55 -0800

    Scripted battle protection

--- script/c200001342.lua
+++ script/c200001342.lua
@@ -4,4 +4,23 @@ function s.initial_effect(c)
 	--Synchro Summon
 	c:EnableReviveLimit()
 	Synchro.AddProcedure(c, aux.FilterBoolFunctionEx(Card.IsType, TYPE_SYNCHRO), 1, 1, Synchro.NonTunerEx(Card.IsType, TYPE_SYNCHRO), 1, 1)
+	--Unaffected
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_IMMUNE_EFFECT)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCondition(s.imcon)
+	--Gain ATK
+	--Revive materials
+	--Negate
+end
+
+function s.imcon(e)
+	local c=e:GetHandler()
+	local bc=c:GetBattleTarget()
+	return bc and bc:IsLevelAbove(5) and bc:IsControler(1-tp)
+end
+
+function s.imfilter(e, te)
+	return te:GetOwnerPlayer()~=e:GetHandlerPlayer()
 end
