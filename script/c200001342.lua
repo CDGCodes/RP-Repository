ed8b6aaaf4774448d7b7456e142f07670d464aa7
Dylan <dpunter8562@gmail.com>
2024-03-08 16:17:57 -0800

    Scripted attack gain effect

--- script/c200001342.lua
+++ script/c200001342.lua
@@ -13,6 +13,13 @@ function s.initial_effect(c)
 	e1:SetValue(s.imfilter)
 	c:RegisterEffect(e1)
 	--Gain ATK
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_ATKCHANGE)
+	e2:SetType(EFFECT_TYPE_TRIGGER_F+EFFECT_TYPE_SINGLE)
+	e2:SetCode(EVENT_PRE_DAMAGE_CALCULATE)
+	e2:SetCondition(s.atkcon)
+	e2:SetOperation(s.atkop)
+	c:RegisterEffect(e2)
 	--Revive materials
 	--Negate
 end
@@ -23,7 +30,23 @@ function s.imcon(e)
 	local tc=Duel.GetAttackTarget()
 	return bc and ((bc==c and tc:IsLevelAbove(5)) or (tc==c and bc:IsLevelAbove(5)))
 end
-
 function s.imfilter(e, te)
 	return te:GetOwnerPlayer()~=e:GetHandlerPlayer()
+end
+
+function s.atkcon(e, tp, eg, ep, ev, re, r, rp)
+	local c=e:GetHandler()
+	return c:GetBattleTarget()
+end
+function s.atkop(e, tp, eg, ep, ev, re, r, rp)
+	local c=e:GetHandler()
+	local bc=c:GetBattleTarget()
+	if c:IsRelateToBattle() and c:IsFaceup() and bc:IsRelateToBattle() and bc:IsFaceup() then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetReset(RESET_PHASE+PHASE_DAMAGE_CAL)
+		e1:SetValue(bc:GetAttack())
+		c:RegisterEffect(e1)
+	end
 end
