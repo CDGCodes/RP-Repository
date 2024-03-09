bf3e17141d6e085f9191db1fabd8591a1f084fa5
Dylan <dpunter8562@gmail.com>
2024-03-09 14:45:58 -0800

    Updated script for equip spell

--- script/Duelist Armaments/c200001343.lua
+++ script/Duelist Armaments/c200001343.lua
@@ -5,7 +5,10 @@ function s.initial_effect(c)
 	--ATK up
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_EQUIP)
-	e2:SetCode(EFFECT_UPDATE_ATTACK+EFFECT_UPDATE_DEFENSE)
+	e2:SetCode(EFFECT_UPDATE_ATTACK)
 	e2:SetValue(1000)
 	c:RegisterEffect(e2)
+	local e3=e2:Clone()
+	e3:SetCode(EFFECT_UPDATE_DEFENSE)
+	c:RegisterEffect(e3)
 end
