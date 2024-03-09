cbff4d16cfa4bd0ece54e55bed361b21ce9fe4cd
Dylan <dpunter8562@gmail.com>
2024-03-09 15:28:36 -0800

    Added Special Summon script for Duelist Armaments

--- script/Duelist Armaments/c200001343.lua
+++ script/Duelist Armaments/c200001343.lua
@@ -11,4 +11,25 @@ function s.initial_effect(c)
 	local e3=e2:Clone()
 	e3:SetCode(EFFECT_UPDATE_DEFENSE)
 	c:RegisterEffect(e3)
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_IGNITION)
+	e4:SetRange(LOCATION_HAND)
+	e4:SetTarget(s.sptg)
+	e4:SetOperation(s.spop)
+	c:RegisterEffect(e4)
+end
+
+function s.sptg(e, tp, eg, ep, ev, re, r, rp, chk)
+	if chk==0 then return Duel.GetLocationCount(tp, LOCATION_MZONE)>0 and Duel.IsPlayerCanSpecialSummonMonster(tp, id, 0xFEDC, 0x21, 1500, 1000, 2, RACE_ILLUSION, ATTRIBUTE_LIGHT) end
+	Duel.SetOperationInfo(0, CATEGORY_SPECIAL_SUMMON, e:GetHandler(), 1, 0, 0)
+end
+
+function s.spop(e, tp, eg, ep, ev, re, r, rp)
+	if Duel.GetLocationCount(tp, LOCATION_MZONE)<=0 then return end
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) and Duel.IsPlayerCanSpecialSummonMonster(tp, id, 0xFEDC, 0x21, 1500, 1000, 2, RACE_ILLUSION, ATTRIBUTE_LIGHT) then
+		c:AddMonsterAttribute(TYPE_EFFECT+TYPE_SPELL)
+		Duel.SpecialSummonStep(c, 0, tp, tp, true, false, POS_FACEUP)
+		c:AddMonsterAttributeComplete()
+	end
 end
