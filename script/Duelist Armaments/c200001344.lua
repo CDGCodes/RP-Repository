6ffdd21a2927c4c2a5455544e72fd141aa21b530
Dylan <dpunter8562@gmail.com>
2024-03-10 21:02:46 -0700

    Added scripts

---
+++ script/Duelist Armaments/c200001344.lua
@@ -0,0 +1,113 @@
+--Duelist Armaments - Sword
+local s, id=GetID()
+function s.initial_effect(c)
+	aux.AddEquipProcedure(c)
+	--ATK/DEF up (Equip)
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_EQUIP)
+	e2:SetCode(EFFECT_UPDATE_ATTACK)
+	e2:SetValue(500)
+	c:RegisterEffect(e2)
+	local e3=e2:Clone()
+	e3:SetCode(EFFECT_UPDATE_DEFENSE)
+	c:RegisterEffect(e3)
+	--Special Summon
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id, 0))
+	e4:SetType(EFFECT_TYPE_IGNITION)
+	e4:SetRange(LOCATION_HAND)
+	e4:SetCountLimit(1, id, EFFECT_COUNT_CODE_OATH)
+	e4:SetTarget(s.sptg)
+	e4:SetOperation(s.spop)
+	c:RegisterEffect(e4)
+	local e5=e4:Clone()
+	e5:SetRange(LOCATION_GRAVE)
+	e5:SetCondition(s.spgycon)
+	c:RegisterEffect(e5)
+	--Copy Equip effects as monster
+	local e6=e2:Clone()
+	e6:SetType(EFFECT_TYPE_SINGLE)
+	e6:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e6:SetRange(LOCATION_MZONE)
+	e6:SetCondition(s.effcon)
+	c:RegisterEffect(e6)
+	local e7=e3:Clone()
+	e7:SetType(EFFECT_TYPE_SINGLE)
+	e7:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e7:SetRange(LOCATION_MZONE)
+	e7:SetCondition(s.effcon)
+	c:RegisterEffect(e7)
+	--Allow direct attacks
+	local e8=Effect.CreateEffect(c)
+	e8:SetType(EFFECT_TYPE_SINGLE)
+	e8:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e8:SetCode(EFFECT_IGNORE_BATTLE_TARGET)
+	e8:SetRange(LOCATION_MZONE)
+	e8:SetValue(1)
+	e8:SetCondition(s.dircon)
+	c:RegisterEffect(e8)
+	--Add card to hand
+	local e9=Effect.CreateEffect(c)
+	e9:SetDescription(aux.Stringid(id,0))
+	e9:SetCategory(CATEGORY_TOHAND)
+	e9:SetType(EFFECT_TYPE_IGNITION)
+	e9:SetRange(LOCATION_HAND)
+	e9:SetCountLimit(1)
+	e9:SetTarget(s.addtg)
+	e9:SetOperation(s.addop)
+	c:RegisterEffect(e9)
+end
+
+function s.sptg(e, tp, eg, ep, ev, re, r, rp, chk)
+	if chk==0 then return Duel.GetLocationCount(tp, LOCATION_MZONE)>0 and Duel.IsPlayerCanSpecialSummonMonster(tp, id, 0xFEDC, 0x21, 1500, 1000, 2, RACE_ILLUSION, ATTRIBUTE_LIGHT) end
+	Duel.SetOperationInfo(0, CATEGORY_SPECIAL_SUMMON, e:GetHandler(), 1, 0, 0)
+end
+function s.splimit(e, c, sump, sumtype, sumpos, targetp, se)
+	if se:GetHandler():IsSpell() then return false end
+	return (c:IsLocation(LOCATION_DECK) or c:IsLocation(LOCATION_HAND))
+end
+function s.spop(e, tp, eg, ep, ev, re, r, rp)
+	if Duel.GetLocationCount(tp, LOCATION_MZONE)<=0 then return end
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) and Duel.IsPlayerCanSpecialSummonMonster(tp, id, 0xFEDC, 0x21, 1500, 1000, 2, RACE_ILLUSION, ATTRIBUTE_LIGHT) then
+		c:AddMonsterAttribute(TYPE_EFFECT+TYPE_SPELL)
+		Duel.SpecialSummonStep(c, 0, tp, tp, true, false, POS_FACEUP)
+		c:AddMonsterAttributeComplete()
+		Duel.SpecialSummonComplete()
+		local e1=Effect.CreateEffect(c)
+		e1:SetDescription(aux.Stringid(id, 1))
+		e1:SetType(EFFECT_TYPE_FIELD)
+		e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
+		e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+		e1:SetTargetRange(1, 0)
+		e1:SetTarget(s.splimit)
+		e1:SetReset(RESET_PHASE+PHASE_END)
+		Duel.RegisterEffect(e1, tp)
+	end
+end
+function s.spgycon(e, tp, eg, ep, ev, re, r, rp)
+	return Duel.GetFieldGroupCount(tp, 0, LOCATION_MZONE)>Duel.GetFieldGroupCount(tp, LOCATION_MZONE, 0)
+end
+
+function s.effcon(e)
+	return e:GetHandler():IsType(TYPE_EFFECT)
+end
+function s.dircon(e)
+	return e:GetHandler():IsAttackPos() and s.effcon
+end
+
+function s.addfilter(c)
+	return c:IsLocation(LOCATION_DECK) and c:IsSetCard(0xFEDC) and c:IsSpell() and c:IsAbleToHand() and not c:IsCode(id)
+end
+function s.addtg(e, tp, eg, ep, ev, re, r, rp, chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.addfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.addop(e, tp, eg, ep, ev, re, r, rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.addfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
