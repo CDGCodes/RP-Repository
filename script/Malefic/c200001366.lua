db4b01dc6d298039fa3717f28d85d403c5fb558d
Dylan <dpunter8562@gmail.com>
2024-04-01 17:37:18 -0700

    Finalized script for Malefic Claw of Hermos

--- script/Malefic/c200001366.lua
+++ script/Malefic/c200001366.lua
@@ -1,1 +1,80 @@
- 
+--Malefic Claw of Hermos
+local s, id=GetID()
+function s.initial_effect(c)
+	--Equip to opponent's monster
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCountLimit(1, id)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCategory(CATEGORY_EQUIP)
+	e1:SetTarget(s.equiptg)
+	e1:SetOperation(s.equipop)
+	c:RegisterEffect(e1)
+end
+
+function s.equiptg(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and chkc:IsFaceup() end
+	if chk==0 then return Duel.GetLocationCount(tp, LOCATION_SZONE)>0 and Duel.IsExistingTarget(Card.IsFaceup, tp, 0, LOCATION_MZONE, 1, nil) and Duel.GetLocationCount(tp, LOCATION_MZONE)>0 end
+	Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_EQUIP)
+	Duel.SelectTarget(tp, Card.IsFaceup, tp, 0, LOCATION_MZONE, 1, 1, nil)
+	Duel.SetOperationInfo(0, CATEGORY_EQUIP, e:GetHandler(), 1, 0, 0)
+end
+function s.eqlimit(e, c)
+	return c==e:GetLabelObject()
+end
+function s.equipop(e, tp, eg, ep, ev, re, r, rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) then return end
+	if c:IsLocation(LOCATION_MZONE) and c:IsFacedown() then return end
+	local tc=Duel.GetFirstTarget()
+	if Duel.GetLocationCount(tp, LOCATION_MZONE)<=0 or tc:IsFacedown() or not tc:IsRelateToEffect(e) then
+		Duel.SendtoGrave(c, REASON_EFFECT)
+		return
+	end
+	Duel.Equip(tp, c, tc, true)
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_EQUIP_LIMIT)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+	e1:SetValue(s.eqlimit)
+	e1:SetLabelObject(tc)
+	c:RegisterEffect(e1)
+	--Take control of equipped monster
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_EQUIP)
+	e2:SetCode(EFFECT_SET_CONTROL)
+	e2:SetValue(tp)
+	e2:SetReset(RESET_EVENT+RESETS_STANDARD)
+	c:RegisterEffect(e2)
+	--Banish equipped monster when leave field
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
+	e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e3:SetCode(EVENT_LEAVE_FIELD_P)
+	e3:SetOperation(s.checkop)
+	e3:SetReset(RESET_EVENT+RESETS_STANDARD)
+	c:RegisterEffect(e3)
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
+	e4:SetCode(EVENT_LEAVE_FIELD)
+	e4:SetOperation(s.banop)
+	e4:SetReset(RESET_EVENT+RESET_OVERLAY+RESET_TOFIELD)
+	e4:SetLabelObject(e3)
+	c:RegisterEffect(e4)
+end
+function s.checkop(e, tp, eg, ep, ev, re, r, rp)
+	if e:GetHandler():IsDisabled() then
+		e:SetLabel(1)
+	else e:SetLabel(0) end
+end
+function s.banop(e, tp, eg, ep, ev, re, r, rp)
+	e:Reset()
+	if e:GetLabelObject():GetLabel()~=0 then return end
+	local tc=e:GetHandler():GetEquipTarget()
+	if tc and tc:IsLocation(LOCATION_MZONE) then
+		Duel.Remove(tc, POS_FACEUP, REASON_EFFECT)
+	end
+end
