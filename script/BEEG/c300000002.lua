b9f8106b973b6892b1c324e4b298da97c7c4ac82
Dylan <dpunter8562@gmail.com>
2024-04-02 11:14:49 -0700

    Finalized search effect

--- script/BEEG/c300000002.lua
+++ script/BEEG/c300000002.lua
@@ -5,16 +5,12 @@
  	e0:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
  	e0:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
  	e0:SetCode(EVENT_SUMMON_SUCCESS)
- 	e0:SetTarget(s.tg)
  	e0:SetOperation(s.op)
  	c:RegisterEffect(e0)
  end
  
- function s.tg(e, tp, eg, ep, ev, re, r, rp)
- 	Duel.SetOperationInfo(0, CATEGORY_TOHAND, nil, 2, tp, LOCATION_DECK)
- end
  function s.filter(c)
- 	return c:IsSetCard(0x2334) and c:IsAbleToHand()
+ 	return c:IsMonster() and c:IsSetCard(0x2334) and c:IsAbleToHand()
  end
  function s.op(e, tp, eg, ep, ev, re, r, rp)
  	local g = Duel.GetMatchingGroup(s.filter, tp, LOCATION_DECK, 0, nil)
