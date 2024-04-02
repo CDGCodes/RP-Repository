42faeb60c910a63769d9f7083d56c82b73e631ac
Dylan <dpunter8562@gmail.com>
2024-04-02 10:35:11 -0700

    Added script for BEEG Habitat

---
+++ script/BEEG/c300000001.lua
@@ -0,0 +1,54 @@
+--BEEG Habitat
+local s, id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_ACTIVATE)
+	e0:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e0)
+	--Summon Restriction
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CANNOT_NEGATE)
+	e1:SetRange(LOCATION_FZONE)
+	e1:SetTargetRange(1,0)
+	e1:SetTarget(s.splimit)
+	c:RegisterEffect(e1)
+	--Indes
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetRange(LOCATION_FZONE)
+	e2:SetCode(EFFECT_INDESTRUCTABLE)
+	e2:SetValue(s.prval)
+	e2:SetCondition(s.prcon)
+	c:RegisterEffect(e2)
+	--Cannot activate/set
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetCode(EFFECT_CANNOT_SSET)
+	e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e3:SetRange(LOCATION_FZONE)
+	e3:SetTargetRange(1,0)
+	e3:SetTarget(s.setlimit)
+	c:RegisterEffect(e3)
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetCode(EFFECT_CANNOT_ACTIVATE)
+	e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e3:SetRange(LOCATION_FZONE)
+	e3:SetTargetRange(1,0)
+	e3:SetValue(s.actlimit)
+	c:RegisterEffect(e3)
+end
+
+function s.splimit(c)
+	return not c:IsSetCard(0x2334)
+end
+
+function s.setlimit(e,c,tp)
+	return c:IsType(TYPE_FIELD)
+end
+function s.actlimit(e,re,tp)
+	return re:IsActiveType(TYPE_FIELD) and re:IsHasType(EFFECT_TYPE_ACTIVATE)
+end
