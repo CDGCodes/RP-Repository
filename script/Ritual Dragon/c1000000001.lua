46e0609d13d7ff730b644c3372d6226e0b4478e4
Kiderobin <djt9100@gmail.com>
2024-04-02 21:19:37 -0400

    The Ritual Spell lists Ritual Dragon

--- script/Ritual Dragon/c1000000001.lua
+++ script/Ritual Dragon/c1000000001.lua
@@ -12,7 +12,7 @@ function s.initial_effect(c)
 	e1:SetOperation(s.spop)
 	c:RegisterEffect(e1)
 end
-
+s.listed_names={1000000000}
 function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():IsAbleToRemoveAsCost() end
 local c=e:GetHandler()
