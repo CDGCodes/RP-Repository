686367176d61c8b282b4cb4d65f835b7aa86a7a5
ScrollProps <scrollprops@gmail.com>
2024-04-04 18:18:38 -0400

    Update c70000000.lua

---
+++ script/Magical Girl/c70000000.lua
@@ -0,0 +1,33 @@
+--Magical Teacher Verte
+local s,id=GetID()
+function s.initial_effect(c)
+--when destroyed you can get magical girl to field
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e3:SetProperty(EFFECT_FLAG_DELAY)
+	e3:SetCode(EVENT_DESTROYED)
+	e3:SetCondition(function(e,tp,eg,ep,ev,re,r,rp) return r&(REASON_BATTLE|REASON_EFFECT)>0 end)
+	e3:SetTarget(s.dmsptg)
+	e3:SetOperation(s.dmspop)
+	c:RegisterEffect(e3)
+	
+end
+
+function s.dmspfilter(c,e,tp)
+	return c:IsSetCard(0x6942) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.dmsptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.dmspfilter,tp,LOCATION_HAND|LOCATION_DECK|LOCATION_GRAVE,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND|LOCATION_DECK|LOCATION_GRAVE)
+end
+function s.dmspop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.dmspfilter),tp,LOCATION_HAND|LOCATION_DECK|LOCATION_GRAVE,0,1,1,nil,e,tp)
+	if #g>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
