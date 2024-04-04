fe40b02caed6c0acc68fe2bb30512e0e8876cc5d
ScrollProps <scrollprops@gmail.com>
2024-04-04 19:53:11 -0400

    EH

---
+++ script/Magical Girl/c70000002.lua
@@ -0,0 +1,13 @@
+--Magical Girl Rouge Lv5
+local s,id=GetID()
+function s.initial_effect(c)
+local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
