269fb02371229a6f3bc97a527785524dac6dd732
ScrollProps <scrollprops@gmail.com>
2024-04-04 18:26:53 -0400

    Update c70000000.lua

--- script/Magical Girl/c70000000.lua
+++ script/Magical Girl/c70000000.lua
@@ -13,6 +13,21 @@ function s.initial_effect(c)
 	e3:SetOperation(s.dmspop)
 	c:RegisterEffect(e3)
 	
+		--atk up
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_ATKCHANGE)
+	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetHintTiming(TIMING_DAMAGE_STEP)
+	e1:SetCondition(s.condition)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+	
 end
 
 function s.dmspfilter(c,e,tp)
@@ -29,5 +44,41 @@ function s.dmspop(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.dmspfilter),tp,LOCATION_HAND|LOCATION_DECK|LOCATION_GRAVE,0,1,1,nil,e,tp)
 	if #g>0 then
 		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
+
+
+
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetCurrentPhase()~=PHASE_DAMAGE or not Duel.IsDamageCalculated()
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return c:IsDiscardable() end
+	Duel.SendtoGrave(c,REASON_COST+REASON_DISCARD)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsSetCard,0x6942),tp,LOCATION_MZONE,0,1,nil) end
+end
+function s.ctfilter(c)
+	return (c:IsLocation(LOCATION_GRAVE) or c:IsFaceup()) and c:IsMonster() and c:IsSetCard(0x6942)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local tg=Duel.GetMatchingGroup(aux.FaceupFilter(Card.IsSetCard,0x6942),tp,LOCATION_MZONE,0,nil)
+	local g=Duel.GetMatchingGroup(s.ctfilter,tp,LOCATION_MZONE+LOCATION_GRAVE,LOCATION_MZONE+LOCATION_GRAVE,nil)
+	if #tg>0 and #g>0 then
+		local d=g:GetClassCount(Card.GetCode)*300
+		local sc=tg:GetFirst()
+		for sc in aux.Next(tg) do
+			local e1=Effect.CreateEffect(e:GetHandler())
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_UPDATE_ATTACK)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+			e1:SetValue(d)
+			sc:RegisterEffect(e1)
+			local e2=e1:Clone()
+			e2:SetCode(EFFECT_UPDATE_DEFENSE)
+			sc:RegisterEffect(e2)
+		end
 	end
 end
