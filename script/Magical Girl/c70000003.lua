30c9320f94372d231b7bf742560b9f39a19712ea
ScrollProps <scrollprops@gmail.com>
2024-04-05 18:26:00 -0400

    Create c70000003.lua

---
+++ script/Magical Girl/c70000003.lua
@@ -0,0 +1,24 @@
+--Magical Girl Jaune Lv1
+local s,id=GetID()
+function s.initial_effect(c)
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e2:SetCode(EVENT_CHAIN_SOLVING)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1)
+	e2:SetCondition(s.negcon)
+	e2:SetOperation(s.negop)
+	c:RegisterEffect(e2)
+	aux.DoubleSnareValidity(c,LOCATION_SZONE)
+end
+
+function s.negcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsRace,RACE_SPELLCASTER),tp,LOCATION_MZONE,0,1,nil)
+		and rp==1-tp and re:IsActiveType(TYPE_TRAP) and Duel.IsChainDisablable(ev)
+end
+function s.negop(e,tp,eg,ep,ev,re,r,rp)
+	local rc=re:GetHandler()
+	if Duel.NegateEffect(ev) and rc:IsRelateToEffect(re) then
+		Duel.Destroy(rc,REASON_EFFECT)
+	end
+end
