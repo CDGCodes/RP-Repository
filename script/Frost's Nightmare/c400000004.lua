599b913c29a08a4643a6185540fac815ffb3ccd9
unknown <roadkill711@gmail.com>
2024-04-05 01:11:23 +0100

    Testing

--- script/Frost's Nightmare/c400000004.lua
+++ script/Frost's Nightmare/c400000004.lua
@@ -13,6 +13,22 @@ function s.initial_effect(c)
 		e1:SetOperation(s.thop)
 		c:RegisterEffect(e1)
 	
+		-- Draw Effect
+	
+		local c = e:GetHandler()
+		local e1=Effect.CreateEffect(c)
+		e2:SetDescription(aux.Stringid(id,0))
+		e2:SetCategory(CATEGORY_DRAW)
+			e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+			e2:SetRange(LOCATION_MZONE)
+			e2:SetProperty(EFFECT_FLAG_OATH)
+			e2:SetCode(EVENT_EVENT_FREE_CHAIN)
+			e2:SetCountLimit(1)
+			e2:SetTarget(s.drtg)
+			e2:SetOperation(s.drop)
+			e2:SetReset(RESET_EVENT+RESETS_STANDARD-(RESET_LEAVE+RESET_TEMP_REMOVE+RESET_TURN_SET)+RESET_PHASE+PHASE_END)
+			c:RegisterEffect(e2)
+	
 end
 
 function s.thfilter(c)
@@ -29,4 +45,16 @@ function s.thop(e,tp,eg,ep,ev,re,r,rp)
 		Duel.SendtoHand(g,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,g)
 	end
+end
+
+--Draw Function
+function s.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDraw(tp,1) end
+	Duel.SetTargetPlayer(tp)
+	Duel.SetTargetParam(1)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
+end
+function s.drop(e,tp,eg,ep,ev,re,r,rp)
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.Draw(p,d,REASON_EFFECT)
 end
