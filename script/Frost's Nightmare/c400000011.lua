647fe184040668652d721ed430994659815c2866
unknown <roadkill711@gmail.com>
2024-04-06 00:56:03 +0100

    Yello

--- script/Frost's Nightmare/c400000011.lua
+++ script/Frost's Nightmare/c400000011.lua
@@ -3,7 +3,7 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--fusion material
 	c:EnableReviveLimit()
-	Fusion.AddProcMixN(c,true,true,40000001,aux.FilterBoolFunctionEx(Card.IsRace,RACE_FIEND))
+	Fusion.AddProcMix(c,true,true,400000001,aux.FilterBoolFunctionEx(Card.IsRace,RACE_FIEND))
 	--Invunrability
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
@@ -13,19 +13,26 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 
 	--Discard 1 to search 1
-	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
-	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e1:SetCode(EVENT_SUMMON_SUCCESS)
-	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
-	e1:SetCountLimit(1,id)
-	e1:SetTarget(s.thtg)
-	e1:SetOperation(s.thop)
-	c:RegisterEffect(e1)
-	local e2=e1:Clone()
-	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1,id,0)
+	e2:SetTarget(s.thtg)
+	e2:SetOperation(s.thop)
 	c:RegisterEffect(e2)
+	
+	
+	--Target 3 gain 3000
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_QUICK_O)
+	e3:SetCode(EVENT_FREE_CHAIN)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCountLimit(1,id,1)
+	e3:SetTarget(s.bgtg)
+	e3:SetOperation(s.bgop)
+	c:RegisterEffect(e3)
 end
 --Invunrability
 function s.prval(e, re, r, rp)
@@ -37,15 +44,56 @@ function s.thfilter(c)
 	return c:IsSetCard(0x1A2B) and c:IsSpellTrap() and c:IsAbleToHand()
 end
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK+LOCATION_GRAVE)
 end
 function s.thop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+	Duel.DiscardHand(tp,aux.True,1,1,REASON_EFFECT+REASON_DISCARD)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil)
 	if #g>0 then
 		Duel.SendtoHand(g,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,g)
-		Duel.DiscardHand(tp,aux.True,1,1,REASON_EFFECT+REASON_DISCARD)
+	end
+	local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_FIELD)
+		e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+		e1:SetCode(EFFECT_CANNOT_ACTIVATE)
+		e1:SetTargetRange(1,0)
+		e1:SetValue(s.aclimit)
+		e1:SetLabel(g:GetFirst():GetCode())
+		e1:SetReset(RESET_PHASE+PHASE_END)
+		Duel.RegisterEffect(e1,tp)
+end
+function s.aclimit(e,re,tp)
+	local code=e:GetLabel()
+	return re:GetHandler():IsCode(code)
+end
+
+--Target 3 gain 3000
+function s.bgfilter(c)
+	return c:IsSetCard(0x1A2B) and (c:IsTrap() or c:IsSpell()) and c:IsAbleToRemove()
+end
+
+function s.bgtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return 
+		Duel.IsExistingMatchingCard(s.bgfilter,tp,LOCATION_GRAVE,0,1,nil) end
+end
+function s.bgop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(s.bgfilter,tp,LOCATION_GRAVE,0,nil)
+	if #g>=1 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
+		local sg=g:Select(tp,1,3,nil)
+		Duel.ConfirmCards(1-tp,sg)
+		Duel.Remove(sg,POS_FACEUP,REASON_EFFECT)
+		Duel.Hint(HINT_SELECTMSG,1-tp,HINTMSG_SET)
+		local e1=Effect.CreateEffect(e:GetHandler())
+	    e1:SetType(EFFECT_TYPE_SINGLE)
+	    e1:SetCode(EFFECT_UPDATE_ATTACK)
+	    e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+	    e1:SetValue(#sg*1000)
+		e:GetHandler():RegisterEffect(e1)
+		
 	end
 end
+
