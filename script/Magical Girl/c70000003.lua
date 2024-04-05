31eed8a713e8828d8728305d9d40fa3d408f1fba
ScrollProps <scrollprops@gmail.com>
2024-04-05 18:30:16 -0400

    Update c70000003.lua

--- script/Magical Girl/c70000003.lua
+++ script/Magical Girl/c70000003.lua
@@ -14,7 +14,7 @@ end
 
 function s.negcon(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsRace,RACE_SPELLCASTER),tp,LOCATION_MZONE,0,1,nil)
-		and rp==1-tp and re:IsActiveType(TYPE_TRAP) and Duel.IsChainDisablable(ev)
+		and rp==1-tp and (re:IsActiveType(TYPE_TRAP) or re:IsActiveType(TYPE_SPELL)) and Duel.IsChainDisablable(ev)
 end
 function s.negop(e,tp,eg,ep,ev,re,r,rp)
 	local rc=re:GetHandler()
