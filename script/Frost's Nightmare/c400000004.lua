cd53baecf6b9171162171b0ec1fc215e75ae5a57
unknown <roadkill711@gmail.com>
2024-04-05 03:36:58 +0100

    she done

--- script/Frost's Nightmare/c400000004.lua
+++ script/Frost's Nightmare/c400000004.lua
@@ -8,26 +8,34 @@ function s.initial_effect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 		e1:SetCode(EVENT_SUMMON_SUCCESS)
 		e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
-		e1:SetCountLimit(1,id)
+		e1:SetCountLimit(1,id,0)
 		e1:SetTarget(s.thtg)
 		e1:SetOperation(s.thop)
 		c:RegisterEffect(e1)
 	
 		-- Draw Effect
-	
-		local c = e:GetHandler()
-		local e1=Effect.CreateEffect(c)
+		local e2=Effect.CreateEffect(c)
 		e2:SetDescription(aux.Stringid(id,0))
 		e2:SetCategory(CATEGORY_DRAW)
-			e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-			e2:SetRange(LOCATION_MZONE)
-			e2:SetProperty(EFFECT_FLAG_OATH)
-			e2:SetCode(EVENT_EVENT_FREE_CHAIN)
-			e2:SetCountLimit(1)
-			e2:SetTarget(s.drtg)
-			e2:SetOperation(s.drop)
-			e2:SetReset(RESET_EVENT+RESETS_STANDARD-(RESET_LEAVE+RESET_TEMP_REMOVE+RESET_TURN_SET)+RESET_PHASE+PHASE_END)
-			c:RegisterEffect(e2)
+		e2:SetType(EFFECT_TYPE_IGNITION)
+		e2:SetRange(LOCATION_MZONE)
+		e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+		e2:SetCode(EVENT_FREE_CHAIN)
+		e2:SetTarget(s.drtg)
+		e2:SetOperation(s.drop)
+		c:RegisterEffect(e2)
+
+		local e3=Effect.CreateEffect(c)
+		e3:SetDescription(aux.Stringid(id,2))
+		e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
+		e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+		e3:SetCode(EVENT_LEAVE_FIELD)
+		e3:SetCountLimit(1,id,1)
+		e3:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+		e3:SetCondition(s.spcon)
+		e3:SetTarget(s.sptg)
+		e3:SetOperation(s.spop)
+	    c:RegisterEffect(e3)
 	
 end
 
@@ -46,15 +54,47 @@ function s.thop(e,tp,eg,ep,ev,re,r,rp)
 		Duel.ConfirmCards(1-tp,g)
 	end
 end
+function s.tdop(e,tp,eg,ep,ev,re,r,rp)
 
+end
 --Draw Function
 function s.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsPlayerCanDraw(tp,1) end
+	local c=e:GetHandler()
+	if chk==0 then return Duel.IsPlayerCanDraw(tp,1) and c:IsAbleToDeck() end
 	Duel.SetTargetPlayer(tp)
 	Duel.SetTargetParam(1)
 	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
 end
 function s.drop(e,tp,eg,ep,ev,re,r,rp)
-	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
-	Duel.Draw(p,d,REASON_EFFECT)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) and c:IsFaceup() and c:IsAbleToDeck() then
+		Duel.SendtoDeck(c,nil,2,REASON_EFFECT)
+		Duel.ShuffleDeck(tp)
+		local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+		Duel.Draw(p,d,REASON_EFFECT) 
+	end
 end
+
+--Special summon
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsPreviousPosition(POS_FACEUP) and c:GetLocation()~=LOCATION_DECK
+end
+function s.spfilter(c,e,tp)
+	return c:IsSetCard(0x1A2B) and c:IsMonster() and c:GetLevel()<=4 and c:IsCanBeSpecialSummoned(e,0,tp,true,true)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.spfilter),tp,LOCATION_DECK,0,1,1,nil,e,tp)
+	if #g>0 then
+		Duel.SpecialSummon(g,0,tp,tp,true,true,POS_FACEUP)
+	end
+end
+	
+	
