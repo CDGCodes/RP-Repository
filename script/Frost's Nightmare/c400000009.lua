6a5bcbbda1562d66bbc04e417b19d8ceb146e3c2
unknown <roadkill711@gmail.com>
2024-04-05 23:14:50 +0100

    hello again

--- script/Frost's Nightmare/c400000009.lua
+++ script/Frost's Nightmare/c400000009.lua
@@ -33,5 +33,11 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	if tc:IsRelateToEffect(e) and Duel.SendtoDeck(tc,nil,2,REASON_EFFECT) then
 		Duel.ShuffleDeck(tp)
+		local e1=Effect.CreateEffect(e:GetHandler())
+	    e1:SetType(EFFECT_TYPE_SINGLE)
+	    e1:SetCode(EFFECT_UPDATE_ATTACK)
+	    e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+	    e1:SetValue(500)
+		e:GetHandler():RegisterEffect(e1)
 	end
 end
