113d905338540c3803209850d27844ad625dc7a2
unknown <roadkill711@gmail.com>
2024-04-06 20:33:27 +0100

    open sesame

--- script/Frost's Nightmare/c400000012.lua
+++ script/Frost's Nightmare/c400000012.lua
@@ -1,51 +1,54 @@
--- Nightmare gene
+-- Kiva, Nightmare patron
 local s,id=GetID()
 function s.initial_effect(c)
 	--fusion material
 	c:EnableReviveLimit()
-	Fusion.AddProcMix(c,true,true,40000003,aux.FilterBoolFunctionEx(Card.IsRace,RACE_FIEND))
-	--Invunrability
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetRange(LOCATION_MZONE)
-	e1:SetCode(EFFECT_INDESTRUCTABLE)
-	e1:SetValue(s.prval)
-	c:RegisterEffect(e1)
+	Fusion.AddProcMix(c,true,true,400000003,aux.FilterBoolFunctionEx(Card.IsRace,RACE_FIEND))
 
-	--Discard 1 to search 1
-	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
-	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e1:SetCode(EVENT_SUMMON_SUCCESS)
-	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
-	e1:SetCountLimit(1,id)
-	e1:SetTarget(s.thtg)
-	e1:SetOperation(s.thop)
-	c:RegisterEffect(e1)
-	local e2=e1:Clone()
-	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
-	c:RegisterEffect(e2)
-end
---Invunrability
-function s.prval(e, re, r, rp)
-	return (r&REASON_EFFECT+REASON_BATTLE)~=0
+    -- if a card is destroyed while you control this card, summon another
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e3:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+	e3:SetCode(EVENT_LEAVE_FIELD)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCountLimit(1,id,0)
+	e3:SetCondition(s.spcon)
+	e3:SetTarget(s.sptg)
+	e3:SetOperation(s.spop)
+	c:RegisterEffect(e3)
 end
 
---Discard 1 to search 1
-function s.thfilter(c)
-	return c:IsSetCard(0x1A2B) and c:IsSpellTrap() and c:IsAbleToHand()
+
+-- if a card is destroyed while you control this card, summon another
+function s.cfilter(c,e,tp)
+	return c:IsSetCard(0x1A2B) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+	
 end
-function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return (r&REASON_DESTROY)==REASON_DESTROY
 end
-function s.thop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
-	if #g>0 then
-		Duel.SendtoHand(g,nil,REASON_EFFECT)
-		Duel.ConfirmCards(1-tp,g)
-		Duel.DiscardHand(tp,aux.True,1,1,REASON_EFFECT+REASON_DISCARD)
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingTarget(s.cfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp)
+	end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectTarget(tp,s.filter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) and Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)>0 then
+
+		local e1=Effect.CreateEffect(e:GetHandler())
+	    e1:SetType(EFFECT_TYPE_SINGLE)
+	    e1:SetCode(EFFECT_CANNOT_ATTACK)
+	    e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+	    tc:RegisterEffect(e1)
 	end
 end
+
+--If "nightmare essence" is on the field when this card is fusion summoned, negate opponent card effects up to the amount of nightmare spell/traps you control
+
