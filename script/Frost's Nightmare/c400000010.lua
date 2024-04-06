87ce9c6f2e5262b7b7ff6e616f78a95fb1fd0cbc
unknown <roadkill711@gmail.com>
2024-04-06 02:11:00 +0100

    hel

---
+++ script/Frost's Nightmare/c400000010.lua
@@ -0,0 +1,47 @@
+-- Nightmare gene
+local s,id=GetID()
+function s.initial_effect(c)
+	--destroy replace
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EFFECT_DESTROY_REPLACE)
+	e1:SetRange(LOCATION_GRAVE)
+	e1:SetTarget(s.reptg)
+	e1:SetValue(s.repval)
+	e1:SetOperation(s.repop)
+	c:RegisterEffect(e1)
+
+
+	--Cannot Special Summon monsters, except "Nightmare" monsters until the end of turn
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,2))
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
+	e2:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e2:SetTargetRange(1,0)
+	e2:SetTarget(function(e,c) return not c:IsCard(0x1A2B) end)
+	e2:SetReset(RESET_PHASE|PHASE_END,2)
+	Duel.RegisterEffect(e2,tp)
+end
+	
+
+--destroy replace
+function s.repfilter(c,tp)
+	return c:IsLocation(LOCATION_MZONE) and c:IsControler(tp) 
+		and not c:IsReason(REASON_REPLACE) and c:IsReason(REASON_EFFECT+REASON_BATTLE)
+end
+function s.reptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsAbleToRemove() and eg:IsExists(s.repfilter,1,nil,tp) end
+	return Duel.SelectEffectYesNo(tp,e:GetHandler(),96)
+end
+function s.repval(e,c)
+	return s.repfilter(c,e:GetHandlerPlayer())
+end
+function s.repop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Remove(e:GetHandler(),POS_FACEUP,REASON_EFFECT)
+end
+
+--splimit
+
+
+	
