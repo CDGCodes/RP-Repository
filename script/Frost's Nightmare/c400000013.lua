4b11e5e767ef714230475b4ab2872b565773aaee
unknown <roadkill711@gmail.com>
2024-04-06 22:32:00 +0100

    ello again 2

--- script/Frost's Nightmare/c400000013.lua
+++ script/Frost's Nightmare/c400000013.lua
@@ -3,5 +3,5 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--fusion material
 	c:EnableReviveLimit()
-	Fusion.AddProcMix(c,true,true,400000003,400000001)
+	Fusion.AddProcMix(c,true,true,400000011,400000012)
 end
