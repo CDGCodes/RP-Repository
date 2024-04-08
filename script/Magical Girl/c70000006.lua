78edacd56ca3a0e7c3e2052fbe5762fc5ff50e94
ScrollProps <scrollprops@gmail.com>
2024-04-08 17:34:47 -0400

    Update c70000006.lua

--- script/Magical Girl/c70000006.lua
+++ script/Magical Girl/c70000006.lua
@@ -4,6 +4,7 @@ function s.initial_effect(c)
 	--Xyz Summon
 	c:EnableReviveLimit()
 	Xyz.AddProcedure(c, nil, 2, 2, nil, nil, 2)
+		local e1=Effect.CreateEffect(c)
 		local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(id,0))
 	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
