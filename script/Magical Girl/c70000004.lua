06ddee484e696fae7a18f71e1b8a12d9d416b193
ScrollProps <scrollprops@gmail.com>
2024-04-08 14:40:26 -0400

    Update c70000004.lua

--- script/Magical Girl/c70000004.lua
+++ script/Magical Girl/c70000004.lua
@@ -15,16 +15,20 @@ function s.initial_effect(c)
 	e1:SetCountLimit(1)
 	e1:SetCost(aux.dxmcostgen(1,1,nil))
 	e1:SetTarget(s.target)
-	e1:SetOperation(s.operation)
+	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1,false,REGISTER_FLAG_DETACH_XMAT)
+end
+
+function s.filter(c)
+	return c:IsType(TYPE_SPELL+TYPE_TRAP) and c:IsFaceup()
 end
 
 
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsOnField() and chkc:IsSpellTrap() end
-	if chk==0 then return Duel.IsExistingTarget(Card.IsType,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil,TYPE_SPELL+TYPE_TRAP) end
+	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_NEGATE)
-	local g=Duel.SelectTarget(tp,Card.IsType,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil,TYPE_SPELL+TYPE_TRAP)
+	local g=Duel.SelectTarget(tp,s.filter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_DISABLE,g,1,0,0)
 end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
@@ -53,31 +57,6 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 		e2:SetValue(RESET_TURN_SET)
 		e2:SetReset(RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_END)
 		tc:RegisterEffect(e2)
-		local pos=Duel.GetChainInfo(0,CHAININFO_TARGET_PARAM)
-		if c:IsRelateToEffect(e) and pos&POS_FACEDOWN>0 then
-			Duel.BreakEffect()
-			c:RegisterFlagEffect(id,RESET_CHAIN,0,0)
-			--Negate Spell/Trap effects in the same column
-			local e3=Effect.CreateEffect(c)
-			e3:SetType(EFFECT_TYPE_FIELD)
-			e3:SetCode(EFFECT_DISABLE)
-			e3:SetTargetRange(LOCATION_ONFIELD,LOCATION_ONFIELD)
-			e3:SetTarget(s.distg)
-			e3:SetReset(RESET_PHASE|PHASE_END)
-			e3:SetLabel(c:GetSequence())
-			Duel.RegisterEffect(e3,tp)
-			local e4=e3:Clone()
-			e4:SetCode(EFFECT_DISABLE_TRAPMONSTER)
-			Duel.RegisterEffect(e4,tp)
-			local e5=Effect.CreateEffect(c)
-			e5:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-			e5:SetCode(EVENT_CHAIN_SOLVING)
-			e5:SetOperation(s.disop)
-			e5:SetReset(RESET_PHASE|PHASE_END)
-			e5:SetLabel(c:GetSequence())
-			Duel.RegisterEffect(e5,tp)
-			local zone=1<<(c:GetSequence()+8)
-			Duel.Hint(HINT_ZONE,tp,zone)
-		end
+		
 	end
 end
