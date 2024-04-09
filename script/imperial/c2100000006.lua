2ba6f635d07c6744ab02cc7dd228c7366e249688
MoonStarX1 <marrkm21@gmail.com>
2024-04-09 18:03:26 -0400

    effect

---
+++ script/imperial/c2100000006.lua
@@ -0,0 +1,32 @@
+--Imperial Destructive dragon 
+local s,id=GetID()
+function s.inital_effect(c)
+    local e1=Effect.CreateEffect(c)
+    e1:SetCategory(CATEGORY_NEGATE+CATEGORY_DESTROY)
+    e1:SetType(EFFECT_TYPE_QUICK_O)
+    e1:SetCode(EVENT_CHAINING)
+    e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
+    e1:SetRange(LOCATION_HAND)
+    e1:SetCondition(s.ngcon)
+    e1:SetTarget(s.ngcon)
+    e1:SetOperation(s.ngop)
+    c1:RegisterEffect(e1)
+end
+function s.ngcon(e,tp,eg,ep,ev,re,r,rp)
+	if re:IsHasCategory(CATEGORY_NEGATE)
+		and Duel.GetChainInfo(ev-1,CHAININFO_TRIGGERING_EFFECT):IsHasType(EFFECT_TYPE_ACTIVATE) then return false end
+	local ex,tg,tc=Duel.GetOperationInfo(ev,CATEGORY_DESTROY)
+	return ex and tg~=nil and tc+tg:FilterCount(Card.IsOnField,nil)-#tg>0
+end
+function s.ngtg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
+	if re:GetHandler():IsDestructable() and re:GetHandler():IsRelateToEffect(re) then
+		Duel.SetOperationInfo(0,CATEGORY_DESTROY,eg,1,0,0)
+    end
+end
+function s.ngop(e,tp,eg,ep,ev,re,r,rp)
+    if Duel.NegateActivation(ev) and re:GetHandler():IsRelateToEffect(re) then
+		Duel.Destroy(eg,REASON_EFFECT)
+    end
+end
