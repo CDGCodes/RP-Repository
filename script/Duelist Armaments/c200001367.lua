c2050d34448deebb5c784ea05efd32a048e9e27d
CDGCodes <dpunter8562@gmail.com>
2024-04-09 10:30:44 -0700

    Slightly adjusted the effect of Armory of the Duelist Armaments

--- script/Duelist Armaments/c200001367.lua
+++ script/Duelist Armaments/c200001367.lua
@@ -93,7 +93,7 @@ function s.spop(e, tp, eg, ep, ev, re, r, rp)
 	end
 	-- Spell Summon Restriction
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id, 1))
+	e1:SetDescription(aux.Stringid(id, 0))
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
 	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
@@ -116,10 +116,27 @@ function s.rttg(e, tp, eg, ep, ev, re, r, rp, chk)
 	if chk==0 then return e:GetHandler():IsAbleToHand() end
 	Duel.SetOperationInfo(0, CATEGORY_TOHAND, nil, 1, tp, LOCATION_GRAVE)
 end
+function s.rtlimit(e, c, sump, sumtype, sumpos, targetp, se)
+	if se:GetHandler():IsSpell() and se:GetHandler():IsSetCard(0xEDC) then return false end
+	return (c:IsLocation(LOCATION_DECK) or c:IsLocation(LOCATION_HAND))
+end
 function s.rtop(e, tp, eg, ep, ev, re, r, rp)
 	local c=e:GetHandler()
 	if c:IsRelateToEffect(e) then
 		Duel.SendtoHand(c, nil, REASON_EFFECT)
 		Duel.ConfirmCards(1-tp, c)
+		if Duel.IsPlayerCanDraw(tp,1) and Duel.SelectYesNo(tp, aux.Stringid(id, 1)) then
+			Duel.Draw(tp, 1, REASON_EFFECT)
+		end
 	end
+	-- Spell Summon Restriction
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id, 0))
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
+	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e1:SetTargetRange(1, 0)
+	e1:SetTarget(s.rtlimit)
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e1, tp)
 end
