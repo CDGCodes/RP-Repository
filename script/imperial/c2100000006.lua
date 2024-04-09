a370c2172435c498f85cd5aa045e4abecb9c5fc1
MoonStarX1 <marrkm21@gmail.com>
2024-04-09 18:16:26 -0400

    effect

--- script/imperial/c2100000006.lua
+++ script/imperial/c2100000006.lua
@@ -8,7 +8,7 @@ function s.initial_effect(c)
     e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
     e1:SetRange(LOCATION_HAND)
     e1:SetCondition(s.ngcon)
-    e1:SetTarget(s.ngcon)
+    e1:SetTarget(s.ngtg)
     e1:SetOperation(s.ngop)
     c:RegisterEffect(e1)
 end
@@ -20,13 +20,16 @@ function s.ngcon(e,tp,eg,ep,ev,re,r,rp)
 end
 function s.ngtg(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then return true end
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
 	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
 	if re:GetHandler():IsDestructable() and re:GetHandler():IsRelateToEffect(re) then
 		Duel.SetOperationInfo(0,CATEGORY_DESTROY,eg,1,0,0)
     end
 end
 function s.ngop(e,tp,eg,ep,ev,re,r,rp)
+    if Duel.SpecialSummon(e:GetHandler(),0,tp,tp,false,false,POS_FACEUP)>0 then
     if Duel.NegateActivation(ev) and re:GetHandler():IsRelateToEffect(re) then
 		Duel.Destroy(eg,REASON_EFFECT)
     end
+end
 end
