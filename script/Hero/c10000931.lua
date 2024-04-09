f2e49decd4d5d574f3748296c821d2a08a7ac80a
finsterking <finsterking@gmail.com>
2024-04-09 17:00:21 -0400

    Fixed Maybe

--- script/Hero/c10000931.lua
+++ script/Hero/c10000931.lua
@@ -1,4 +1,115 @@
---Devoted Wroughtweiler
-function c10000931.initial_effect(c)
-	
+local s,id=GetID()
+function s.initial_effect(c)
+    --Always treated as an "Elemental HERO" monster
+    c:AddSetcodesRule(0x3008)
+
+    --Special Summon from hand
+    local e1=Effect.CreateEffect(c)
+    e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e1:SetType(EFFECT_TYPE_IGNITION)
+    e1:SetRange(LOCATION_HAND)
+    e1:SetCondition(s.spcon1)
+    e1:SetTarget(s.sptg1)
+    e1:SetOperation(s.spop1)
+    c:RegisterEffect(e1)
+
+    --Quick Effect to Special Summon from GY
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,1))
+    e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e2:SetType(EFFECT_TYPE_QUICK_O)
+    e2:SetCode(EVENT_FREE_CHAIN)
+    e2:SetRange(LOCATION_MZONE)
+    e2:SetCountLimit(1)
+    e2:SetCost(s.qecost)
+    e2:SetTarget(s.qetarget)
+    e2:SetOperation(s.qeoperation)
+    c:RegisterEffect(e2)
+
+    --Replace destruction
+    local e3=Effect.CreateEffect(c)
+    e3:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
+    e3:SetCode(EFFECT_DESTROY_REPLACE)
+    e3:SetRange(LOCATION_GRAVE)
+    e3:SetTarget(s.reptg)
+    e3:SetValue(s.repval)
+    e3:SetOperation(s.repop)
+    c:RegisterEffect(e3)
+end
+
+--Special Summon condition from hand
+function s.spcon1(e,tp,eg,ep,ev,re,r,rp)
+    return Duel.IsExistingMatchingCard(s.spcfilter,tp,LOCATION_MZONE+LOCATION_HAND,0,1,nil)
+end
+
+--Special Summon target from hand
+function s.sptg1(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+end
+
+--Special Summon operation from hand
+function s.spop1(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    if c:IsRelateToEffect(e) and Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)~=0 then
+        -- Add Polymerization or Fusion recovery effect here if needed
+    end
+end
+
+function s.qecost(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return e:GetHandler():IsReleasable() end
+    Duel.Release(e:GetHandler(),REASON_COST)
+end
+
+function s.qetarget(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+    if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and s.qefilter(chkc,e,tp) end
+    if chk==0 then return Duel.IsExistingTarget(s.qefilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+    local g=Duel.SelectTarget(tp,s.qefilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
+end
+
+function s.qefilter(c,e,tp)
+    return c:IsSetCard(0x3008) and c:IsLevelBelow(6) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+
+function s.qeoperation(e,tp,eg,ep,ev,re,r,rp)
+    local tc=Duel.GetFirstTarget()
+    if tc and tc:IsRelateToEffect(e) then
+        Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
+        local e1=Effect.CreateEffect(e:GetHandler())
+        e1:SetType(EFFECT_TYPE_SINGLE)
+        e1:SetCode(EFFECT_CANNOT_TRIGGER)
+        e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+        tc:RegisterEffect(e1)
+        local e2=Effect.CreateEffect(e:GetHandler())
+        e2:SetType(EFFECT_TYPE_SINGLE)
+        e2:SetCode(EFFECT_CANNOT_ATTACK)
+        e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+        tc:RegisterEffect(e2)
+    end
+end
+
+function s.spcfilter(c)
+    return (c:IsCode(57116033) or (c:IsType(TYPE_FUSION) and c:IsSetCard(0x3008)))
+        and c:IsMonster() and (c:IsFaceup() or c:IsLocation(LOCATION_HAND))
+        and c:IsAbleToRemoveAsCost() and aux.SpElimFilter(c,true,true)
+end
+
+function s.repfilter(c,tp)
+    return c:IsFaceup() and c:IsSetCard(0x3008) and c:IsLocation(LOCATION_MZONE) and c:IsControler(tp)
+        and not c:IsReason(REASON_REPLACE) and c:IsReason(REASON_BATTLE)
+end
+
+function s.reptg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return eg:IsExists(s.repfilter,1,nil,tp) and e:GetHandler():IsAbleToRemove() end
+    return Duel.SelectEffectYesNo(tp,e:GetHandler(),96)
+end
+
+function s.repval(e,c)
+    return s.repfilter(c,e:GetHandlerPlayer())
+end
+
+function s.repop(e,tp,eg,ep,ev,re,r,rp)
+    Duel.Remove(e:GetHandler(),POS_FACEUP,REASON_EFFECT)
 end
