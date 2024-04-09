518c35612cf43b8af222a85ada1432c255e18b82
unknown <roadkill711@gmail.com>
2024-04-09 11:15:53 +0100

    Ancient gear customs

--- script/Frost's Nightmare/c400000015.lua
+++ script/Frost's Nightmare/c400000015.lua
@@ -1,69 +1,51 @@
 -- Nightmare essence
 local s,id=GetID()
 function s.initial_effect(c)
-	--Activate
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetDescription(aux.Stringid(id, 0))
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetTarget(s.target)
-	e1:SetOperation(s.activate)
+	e1:SetCountLimit(1, id, EFFECT_COUNT_CODE_OATH)
+	e1:SetCost(s.spcost)
+	e1:SetTarget(s.sptg)
+	e1:SetOperation(s.spop)
 	c:RegisterEffect(e1)
 end
 
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.filter(chkc) end
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 
-		and Duel.IsExistingTarget(s.filter,tp,LOCATION_MZONE,0,1,nil)
-		and Duel.IsPlayerCanSpecialSummonMonster(tp,id,0,0x21,0,0,nil,RACE_PLANT,ATTRIBUTE_EARTH) end
-	local g=Duel.SelectTarget(tp,s.filter,tp,LOCATION_MZONE,0,1,1,nil)
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+
+function s.spcstfilter(c)
+	return c:IsSpell() 
 end
-function s.activate(e,tp,eg,ep,ev,re,r,rp)
+
+function s.spcost(e, tp, eg, ep, ev, re, r, rp, chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.spchkfilter, tp, LOCATION_HAND, 0, 1, nil, e) end
+end
+
+function s.sptg(e, tp, eg, ep, ev, re, r, rp, chk)
+	if chk==0 then return Duel.GetLocationCount(tp, LOCATION_MZONE)>0 and Duel.IsPlayerCanSpecialSummonMonster(tp, id, 0x1A2B, 0x21, 500, 1000, 4, RACE_FIEND, ATTRIBUTE_DARK) end
+	Duel.SetOperationInfo(0, CATEGORY_SPECIAL_SUMMON, e:GetHandler(), 1, 0, 0)
+end
+
+function s.splimit(e, c, sump, sumtype, sumpos, targetp, se)
+	if se:GetHandler():IsSpell() then return false end
+	return (c:IsLocation(LOCATION_DECK) or c:IsLocation(LOCATION_HAND))
+end
+function s.spop(e, tp, eg, ep, ev, re, r, rp)
+	if Duel.GetLocationCount(tp, LOCATION_MZONE)<=0 then return end
 	local c=e:GetHandler()
-	local tc=Duel.GetFirstTarget()
-	if not c:IsRelateToEffect(e) or not tc:IsRelateToEffect(e) then return end
-	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0
-		or not Duel.IsPlayerCanSpecialSummonMonster(tp,id,0,0x11,0,0,1,RACE_FIEND,ATTRIBUTE_DARK) then return end
-	c:AddMonsterAttribute(TYPE_EFFECT+TYPE_SPELL+TYPE_TRAP)
-	Duel.SpecialSummon(c,0,tp,tp,true,false,POS_FACEUP)
-	c:AddMonsterAttributeComplete()
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-	e1:SetRange(LOCATION_MZONE)
-	e1:SetCode(EFFECT_UNRELEASABLE_SUM)
-	e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-	e1:SetValue(1)
-	c:RegisterEffect(e1)
-	local e2=e1:Clone()
-	e2:SetCode(EFFECT_UNRELEASABLE_NONSUM)
-	c:RegisterEffect(e2)
-	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_SINGLE)
-	e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-	e3:SetCode(EFFECT_CANNOT_BE_FUSION_MATERIAL)
-	e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-	e3:SetReset(RESET_EVENT+RESETS_STANDARD)
-	e3:SetValue(1)
-	c:RegisterEffect(e3)
-	local e4=e3:Clone()
-	e4:SetCode(EFFECT_CANNOT_BE_SYNCHRO_MATERIAL)
-	c:RegisterEffect(e4)
-	local e5=Effect.CreateEffect(c)
-	e5:SetType(EFFECT_TYPE_SINGLE)
-	e5:SetCode(EFFECT_CANNOT_ATTACK)
-	e5:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-	e5:SetReset(RESET_EVENT+RESETS_STANDARD)
-	c:RegisterEffect(e5)
-	local e6=Effect.CreateEffect(c)
-	e6:SetType(EFFECT_TYPE_SINGLE)
-	e6:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-	e6:SetReset(RESET_EVENT+RESETS_STANDARD)
-	e6:SetCode(EFFECT_REMOVE_TYPE)
-	e6:SetValue(TYPE_TRAP)
-	c:RegisterEffect(e6)
+	if c:IsRelateToEffect(e) and Duel.IsPlayerCanSpecialSummonMonster(tp, id, 0x1A2B, 0x21, 500, 1000, 4, RACE_FIEND, ATTRIBUTE_DARK) then
+		c:AddMonsterAttribute(TYPE_EFFECT+TYPE_SPELL+TYPE_TRAPMONSTER)
+		Duel.SpecialSummon(c, 0, tp, tp, true, false, POS_FACEUP)
+		c:AddMonsterAttributeComplete()
+		Duel.SpecialSummonComplete()
+		local e1=Effect.CreateEffect(c)
+		e1:SetDescription(aux.Stringid(id, 1))
+		e1:SetType(EFFECT_TYPE_FIELD)
+		e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
+		e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+		e1:SetTargetRange(1, 0)
+		e1:SetTarget(s.splimit)
+		e1:SetReset(RESET_PHASE+PHASE_END)
+		Duel.RegisterEffect(e1, tp)
+	end
 end
-
