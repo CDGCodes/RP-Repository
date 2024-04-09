ca077e0f616f028c1f6d3800b1cc85d4c8bd127f
unknown <roadkill711@gmail.com>
2024-04-09 11:57:04 +0100

    Nightmare codes

--- script/Frost's Nightmare/c400000015.lua
+++ script/Frost's Nightmare/c400000015.lua
@@ -1,4 +1,4 @@
--- Nightmare essence
+-- Nightmare Droid
 local s,id=GetID()
 function s.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
@@ -10,6 +10,19 @@ function s.initial_effect(c)
 	e1:SetTarget(s.sptg)
 	e1:SetOperation(s.spop)
 	c:RegisterEffect(e1)
+
+	--if used for a fusion
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetCode(EVENT_BE_MATERIAL)
+	e2:SetCountLimit(1,{id,1})
+	e2:SetCondition(s.spcon)
+	e2:SetTarget(s.sptg)
+	e2:SetOperation(s.spop)
+	c:RegisterEffect(e2)
 end
 
 
@@ -47,5 +60,37 @@ function s.spop(e, tp, eg, ep, ev, re, r, rp)
 		e1:SetTarget(s.splimit)
 		e1:SetReset(RESET_PHASE+PHASE_END)
 		Duel.RegisterEffect(e1, tp)
+	end
+end
+
+--if used for a fusion
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return (r&REASON_FUSION)==REASON_FUSION 
+		and c:IsLocation(LOCATION_GRAVE) and c:IsFaceup() and c:GetReasonCard():IsOriginalSetCard(0x1A2B)
+end
+function s.spfilter(c,e,tp)
+	return c:IsSetCard(0x1A2B) 
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_GRAVE) and s.spfilter(chkc,e,tp) end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingTarget(s.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectTarget(tp,s.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+	Duel.SetOperationInfo(0,CATEGORY_ATKCHANGE,g,1,0,0)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	local c=e:GetHandler()
+	local fc=c:GetReasonCard()
+	if tc then
+		local e1=Effect.CreateEffect(e:GetHandler())
+	    e1:SetType(EFFECT_TYPE_SINGLE)
+	    e1:SetCode(EFFECT_UPDATE_ATTACK)
+	    e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+	    e1:SetValue(tc:GetAttack()/2)
+		fc:RegisterEffect(e1)
+		
 	end
 end
