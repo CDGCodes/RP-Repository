93c5b709bca909615dc6be1bb86b48568b846bf5
unknown <roadkill711@gmail.com>
2024-04-09 12:10:29 +0100

    Nightmare codes 2

--- script/Frost's Nightmare/c400000015.lua
+++ script/Frost's Nightmare/c400000015.lua
@@ -19,9 +19,9 @@ function s.initial_effect(c)
 	e2:SetProperty(EFFECT_FLAG_DELAY)
 	e2:SetCode(EVENT_BE_MATERIAL)
 	e2:SetCountLimit(1,{id,1})
-	e2:SetCondition(s.spcon)
-	e2:SetTarget(s.sptg)
-	e2:SetOperation(s.spop)
+	e2:SetCondition(s.fscon)
+	e2:SetTarget(s.fstg)
+	e2:SetOperation(s.fsop)
 	c:RegisterEffect(e2)
 end
 
@@ -39,48 +39,35 @@ function s.sptg(e, tp, eg, ep, ev, re, r, rp, chk)
 	Duel.SetOperationInfo(0, CATEGORY_SPECIAL_SUMMON, e:GetHandler(), 1, 0, 0)
 end
 
-function s.splimit(e, c, sump, sumtype, sumpos, targetp, se)
-	if se:GetHandler():IsSpell() then return false end
-	return (c:IsLocation(LOCATION_DECK) or c:IsLocation(LOCATION_HAND))
-end
 function s.spop(e, tp, eg, ep, ev, re, r, rp)
 	if Duel.GetLocationCount(tp, LOCATION_MZONE)<=0 then return end
 	local c=e:GetHandler()
 	if c:IsRelateToEffect(e) and Duel.IsPlayerCanSpecialSummonMonster(tp, id, 0x1A2B, 0x21, 500, 1000, 4, RACE_FIEND, ATTRIBUTE_DARK) then
-		c:AddMonsterAttribute(TYPE_EFFECT+TYPE_SPELL+TYPE_TRAPMONSTER)
+		c:AddMonsterAttribute(TYPE_EFFECT+TYPE_SPELL+TYPE_TRAPMONSTER+TYPE_MONSTER)
 		Duel.SpecialSummon(c, 0, tp, tp, true, false, POS_FACEUP)
 		c:AddMonsterAttributeComplete()
 		Duel.SpecialSummonComplete()
-		local e1=Effect.CreateEffect(c)
-		e1:SetDescription(aux.Stringid(id, 1))
-		e1:SetType(EFFECT_TYPE_FIELD)
-		e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
-		e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
-		e1:SetTargetRange(1, 0)
-		e1:SetTarget(s.splimit)
-		e1:SetReset(RESET_PHASE+PHASE_END)
-		Duel.RegisterEffect(e1, tp)
 	end
 end
 
 --if used for a fusion
-function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+function s.fscon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	return (r&REASON_FUSION)==REASON_FUSION 
 		and c:IsLocation(LOCATION_GRAVE) and c:IsFaceup() and c:GetReasonCard():IsOriginalSetCard(0x1A2B)
 end
-function s.spfilter(c,e,tp)
+function s.fsfilter(c,e,tp)
 	return c:IsSetCard(0x1A2B) 
 end
-function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_GRAVE) and s.spfilter(chkc,e,tp) end
+function s.fstg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_GRAVE) and s.fsfilter(chkc,e,tp) end
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingTarget(s.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+		and Duel.IsExistingTarget(s.fsfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectTarget(tp,s.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+	local g=Duel.SelectTarget(tp,s.fsfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
 	Duel.SetOperationInfo(0,CATEGORY_ATKCHANGE,g,1,0,0)
 end
-function s.spop(e,tp,eg,ep,ev,re,r,rp)
+function s.fsop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	local c=e:GetHandler()
 	local fc=c:GetReasonCard()
