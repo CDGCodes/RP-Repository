e6e406b48a3674b23bd119d8bb6a0c69ebda03e3
ScrollProps <scrollprops@gmail.com>
2024-04-13 18:11:11 -0400

    Update c70000012.lua

--- script/Magical Girl/c70000012.lua
+++ script/Magical Girl/c70000012.lua
@@ -18,7 +18,11 @@ function s.initial_effect(c)
 		function s.ovfilter(c,tp,lc)
 	return c:IsFaceup() and c:IsSummonCode(lc,SUMMON_TYPE_XYZ,tp,70000011)
 end
-	
+
+function s.spfilter(c,e,tp)
+	return c:IsSetCard(0x6942) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+
 	function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local b1=Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp) and e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
@@ -37,9 +41,8 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	local tc=nil
 	if e:GetLabel()==0 then
-		tc=Duel.GetFirstMatchingCard(s.spfilter,tp,LOCATION_HAND,0,nil,e,tp)
 	else
-		tc=Duel.GetFirstMatchingCard(s.spfilter,tp,LOCATION_DECK,0,nil,e,tp)
+		tc=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
 	end
 	if tc then
 		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
