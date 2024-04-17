47238c1c4c87c871d67ffce834ad1dce5105fc02
ScrollProps <scrollprops@gmail.com>
2024-04-17 12:28:59 -0400

    Update c70000014.lua

--- script/Magical Girl/c70000014.lua
+++ script/Magical Girl/c70000014.lua
@@ -26,6 +26,6 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
 	if #g>0 then
-		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
+
 	end
 end
