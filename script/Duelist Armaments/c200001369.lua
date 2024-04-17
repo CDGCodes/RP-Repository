3104e4e59e4eb6d80a4a666507b47830e9d9f6be
CDGCodes <dpunter8562@gmail.com>
2024-04-16 17:13:59 -0700

    Finished Fusion effect of Gauntlet

--- script/Duelist Armaments/c200001369.lua
+++ script/Duelist Armaments/c200001369.lua
@@ -9,25 +9,43 @@ function s.initial_effect(c)
 	e1:SetCode(EFFECT_UPDATE_ATTACK)
 	e1:SetValue(500)
 	c:RegisterEffect(e1)
-	local e3=e2:Clone()
+	local e2=e1:Clone()
 	e2:SetCode(EFFECT_UPDATE_DEFENSE)
 	c:RegisterEffect(e2)
-    local params = {nil,Fusion.OnFieldMat,s.fextra}
-	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,1))
-	e2:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON)
-	e2:SetType(EFFECT_TYPE_QUICK_O)
-	e2:SetRange(LOCATION_ONFIELD)
-    e2:SetCondition(function(e) return e:GetHandler():IsType(TYPE_EFFECT) or e:GetHandler():GetEquipTarget())
-	e2:SetCountLimit(1)
-	e2:SetTarget(Fusion.SummonEffTG(table.unpack(params)))
-	e2:SetOperation(Fusion.SummonEffOP(table.unpack(params)))
-	c:RegisterEffect(e2)
+    local params = {nil,Fusion.OnFieldMat,s.fextra, nil, nil, s.stage2}
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON)
+	e3:SetType(EFFECT_TYPE_QUICK_O)
+	e3:SetCode(EVENT_FREE_CHAIN)
+	e3:SetRange(LOCATION_ONFIELD)
+    e3:SetCondition(function(e) return e:GetHandler():IsType(TYPE_EFFECT) or e:GetHandler():GetEquipTarget() end)
+	e3:SetCountLimit(1, {id, 0})
+	e3:SetTarget(Fusion.SummonEffTG(table.unpack(params)))
+	e3:SetOperation(Fusion.SummonEffOP(table.unpack(params)))
+	c:RegisterEffect(e3)
 end
 
+function s.splimit(e, c, sump, sumtype, sumpos, targetp, se)
+	if se:GetHandler():IsSpell() then return false end
+	return (c:IsLocation(LOCATION_DECK) or c:IsLocation(LOCATION_HAND))
+end
 function s.checkmat(tp,sg,fc)
 	return fc:IsSetCard(0xFEDC) or not sg:IsExists(Card.IsLocation,1,nil,LOCATION_SZONE)
 end
 function s.fextra(e,tp,mg)
-	return Duel.GetMatchingGroup(Fusion.IsMonsterFilter(Card.IsAbleToGrave),tp,LOCATION_SZONE,0,nil),s.checkmat
+	return Duel.GetMatchingGroup(Card.IsAbleToGrave,tp,LOCATION_SZONE,0,nil),s.checkmat
+end
+function s.stage2(e,tc,tp,mg,chk)
+	if chk==2 then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetDescription(aux.Stringid(id, 1))
+		e1:SetType(EFFECT_TYPE_FIELD)
+		e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
+		e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+		e1:SetTargetRange(1, 0)
+		e1:SetTarget(s.splimit)
+		e1:SetReset(RESET_PHASE+PHASE_END)
+		Duel.RegisterEffect(e1, tp)
+	end
 end
