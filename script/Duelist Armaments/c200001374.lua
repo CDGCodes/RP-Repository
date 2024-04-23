db79be801c06fd4421a8048e79800ac4cad6225c
CDGCodes <dpunter8562@gmail.com>
2024-04-22 19:04:57 -0700

    Added summon condition for White Armor

---
+++ script/Duelist Armaments/c200001374.lua
@@ -0,0 +1,89 @@
+--Duelist Armaments - White Armor
+local s, id=GetID()
+function s.initial_effect(c)
+    --Summon Restrictions
+	c:SetUniqueOnField(1, 0, s.armfusfilter, LOCATION_MZONE, c)
+	c:EnableReviveLimit()
+	Synchro.AddProcedure(c, nil, 1, 1, Synchro.NonTunerEx(Card.IsType,TYPE_SPELL), 1, 99, s.syncex, nil, nil, s.syncheck)
+	c:SetSPSummonOnce(id)
+    --Add Equip on Summon
+    --Banish card, copy Spell/Trap
+    --Equip card from grave
+	local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,2))
+	e2:SetCategory(CATEGORY_EQUIP)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetCountLimit(1, {id, 1})
+	e2:SetCondition(s.geqcon)
+	e2:SetTarget(s.geqtgt)
+	e2:SetOperation(s.geqop)
+	c:RegisterEffect(e2)
+	aux.AddEREquipLimit(c, nil, aux.True, function(c, e, tp, tc) c:EquipByEffectAndLimitRegister(e, tp, tc, id, true) end, e2)
+    --Gains ATK equal to ATK of equipped monsters
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e3:SetCode(EFFECT_UPDATE_ATTACK)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetValue(s.atkval)
+	c:RegisterEffect(e3)
+	--Return to Extra Deck is it leaves the field
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_SINGLE)
+	e4:SetCode(EFFECT_LEAVE_FIELD_REDIRECT)
+	e4:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e4:SetCondition(function(e)return e:GetHandler():IsFaceup()end)
+	e4:SetValue(LOCATION_DECKBOT)
+	c:RegisterEffect(e4)
+end
+
+function s.armfusfilter(c)
+	return c:IsSpell() and c:IsType(TYPE_FUSION+TYPE_XYZ+TYPE_SYNCHRO)
+end
+
+function s.syncex(c, scard, sumtype, tp)
+    return c:IsType(TYPE_SPELL, scard, sumtype, tp)
+end
+function s.syncfilter(c)
+    return c:IsCode(200001369)
+end
+function s.syncheck(g, sc, tp)
+    return g:IsExists(s.syncfilter, 1, nil)
+end
+
+function s.geqcon(e, tp, eg, ep, ev, re, r, rp)
+	return Duel.GetLocationCount(tp, LOCATION_SZONE)>0
+end
+function s.gfeqfilter(c, sc)
+    return sc:CheckEquipTarget(c)
+end
+function s.geqfilter(c, e, tp)
+	if not c:CheckUniqueOnField(tp) then return false end
+	if c:IsType(TYPE_EQUIP) then return Duel.IsExistingMatchingCard(s.gfeqfilter, tp, LOCATION_MZONE, 0, 1, nil, c) end
+	return c:IsType(TYPE_MONSTER)
+end
+function s.geqtgt(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
+	if chkc then return true end
+	if chk==0 then return Duel.IsExistingTarget(s.geqfilter, tp, LOCATION_GRAVE, LOCATION_GRAVE, 1, nil, e, tp) end
+	local sg=Duel.SelectTarget(tp, s.geqfilter, tp, LOCATION_GRAVE, LOCATION_GRAVE, 1, 1, nil, e, tp)
+	Duel.SetOperationInfo(0, CATEGORY_EQUIP, sq, 1, 0, 0)
+end
+function s.geqop(e, tp, eg, ep, ev, re, r, rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+        local g=Duel.GetMatchingGroup(s.gfeqfilter, tp, LOCATION_MZONE, 0, nil, tc)
+        local sc=g:Select(tp, 1, 1, nil)
+		sc:GetFirst():EquipByEffectAndLimitRegister(e, tp, tc, id)
+	end
+end
+
+function s.atkvalfilter(c)
+	return c:IsOriginalType(TYPE_MONSTER)
+end
+function s.atkval(e, c)
+	return e:GetHandler():GetEquipGroup():Filter(s.atkvalfilter, nil):GetSum(Card.GetTextAttack)
+end
