8ca79e65c330f58ae28206647c1a8dde4ad6e186
CDGCodes <dpunter8562@gmail.com>
2024-04-24 22:43:05 -0700

    Fixed Dual Wield being able to activate without a cost

--- script/Duelist Armaments/c200001350.lua
+++ script/Duelist Armaments/c200001350.lua
@@ -12,8 +12,11 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 end
 
+function s.spchkfilter(c, e)
+	return c:IsSpell() and c:IsDiscardable() and c~=e:GetHandler()
+end
 function s.spcstfilter(c)
-	return c:IsSpell() and c:IsDiscardable()
+	return c:IsSpell() and c:IsDiscardable() and 
 end
 function s.spcost(e, tp, eg, ep, ev, re, r, rp, chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.spchkfilter, tp, LOCATION_HAND, 0, 1, nil, e) end
