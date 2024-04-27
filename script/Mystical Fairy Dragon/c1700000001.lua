d6c0573ed364637ee319e71bb052e241b8161222
BionicPlayz <tsanderson2002@gmail.com>
2024-04-26 22:46:31 -0400

    Add MFD Eff 2

--- script/Mystical Fairy Dragon/c1700000001.lua
+++ script/Mystical Fairy Dragon/c1700000001.lua
@@ -8,22 +8,53 @@ function s.initial_effect(c)
     e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
     e1:SetCode(EVENT_SPSUMMON_SUCCESS)
     e1:SetCondition(function(e) return e:GetHandler():IsSummonType(SUMMON_TYPE_SYNCHRO) end)
+    e1:SetCountLimit(1,{id,0})
     e1:SetTarget(s.ftg)
     e1:SetOperation(s.fop)
     c:RegisterEffect(e1)
+    local e2=Effect.CreateEffect(c)
+    e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_QUICK_O)
+    e2:SetCode(EVENT_CHAINING)
+    e2:SetRange(LOCATION_MZONE)
+    e2:SetCountLimit(1,{id,1})
+    e2:SetCondition(s.spcon)
+    e2:SetTarget(s.sptg)
+    e2:SetOperation(s.spop)
+    c:RegisterEffect(e2)
 end
 function s.ffilter(c)
     return c:IsType(TYPE_FIELD) and c:IsAbleToHand()
 end
 function s.ftg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk ==0 then return Duel.IsExistingMatchingCard(s.ffilter, tp, LOCATION_DECK|LOCATION_GRAVE, 0, 1, nil) end 
+    if chk==0 then return true end
     Duel.SetOperationInfo(0, CATEGORY_TOHAND, nil, 1, tp, LOCATION_DECK|LOCATION_GRAVE)
 end
 function s.fop(e,tp,eg,ep,ev,re,r,rp)
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-    local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.ffilter), tp, LOCATION_DECK|LOCATION_GRAVE, 0, 1, 1,nil)
-    if #g >0 then
-        Duel.SendtoHand(g, nil, REASON_EFFECT)
-        Duel.ConfirmCards(1-tp, g)
+    if Duel.IsExistingMatchingCard(s.ffilter, tp, LOCATION_DECK|LOCATION_GRAVE, 0, 1, nil) then
+        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+        local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.ffilter), tp, LOCATION_DECK|LOCATION_GRAVE, 0, 1, 1,nil)
+        if #g >0 then
+            Duel.SendtoHand(g, nil, REASON_EFFECT)
+           Duel.ConfirmCards(1-tp, g)
+        end
+    end
+end
+function s.spfilter(c,e,tp)
+    return c:IsLevelBelow(4) and c:IsCanBeSpecialSummoned(e, 0, tp, false, false)
+end
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+    local tg=Duel.GetChainInfo(ev)
+    return re:IsHasType(EFFECT_TYPE_ACTIVATE) and tg and tg:IsContains(c) and re:GetHandler():IsType(TYPE_FIELD) and Duel.IsChainDisablable(ev)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.GetLocationCount(tp, LOCATION_MZONE)>0 and Duel.IsExistingMatchingCard(s.spfilter, tp, LOCATION_HAND|LOCATION_GRAVE, 0, 1, nil, e, tp) end
+    Duel.SetOperationInfo(0, CATEGORY_SPECIAL_SUMMON, nil, 1, tp, LOCATION_HAND|LOCATION_GRAVE)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+    if Duel.GetLocationCount(tp, LOCATION_MZONE)<=0 then return end
+    Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_SPSUMMON)
+    local g=Duel.SelectMatchingCard(tp, aux.NecroValleyFilter(s.spfilter), tp, LOCATION_HAND|LOCATION_GRAVE, 0, 1, 1, nil, e, tp)
+    if #g>0 then
+        Duel.SpecialSummon(g, 0, tp, tp, false, false, POS_FACEUP)
     end
 end
