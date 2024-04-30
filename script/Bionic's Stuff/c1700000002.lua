bd6f86084a82cfc3c1f73ce45a9b0d1fe89687e8
BionicPlayz <tsanderson2002@gmail.com>
2024-04-30 00:28:57 -0400

    Raiden Eff1

--- script/Bionic's Stuff/c1700000002.lua
+++ script/Bionic's Stuff/c1700000002.lua
@@ -22,15 +22,14 @@ function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then
         return Duel.GetCustomActivityCount(id, tp, ACTIVITY_SPSUMMON)
     end
-    local c=e:GetHandler()
-    local e1=Effect.CreateEffect(c)
+    local e1=Effect.CreateEffect(e:GetHandler())
     e1:SetDescription(aux.Stringid(id,2))
     e1:SetType(EFFECT_TYPE_FIELD)
     e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_OATH+EFFECT_FLAG_CLIENT_HINT)
     e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
     e1:SetReset(RESET_PHASE|PHASE_END)
     e1:SetTargetRange(1,0)
-    e1:SetTarget(function(_,c) return c:IsAttribute(ATTRIBUTE_LIGHT) or c:IsAttribute(ATTRIBUTE_DARK) end)
+    e1:SetTarget(function(_,c) return not s.spfilter(c) end)
     Duel.RegisterEffect(e1,tp)
 end
 function s.spfilter2(c,e,tp)
@@ -44,9 +43,9 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
     if Duel.GetLocationCount(tp, LOCATION_MZONE)<=0 then return end
     Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_SPSUMMON)
     local g=Duel.SelectMatchingCard(tp, s.spfilter2, tp, LOCATION_HAND|LOCATION_DECK, 0, 1, 1, nil, e, tp)
+    local tc=g:GetFirst()
     if #g>0 and Duel.SpecialSummonStep(g, 0, tp, tp, false, false, POS_FACEUP) then
-        local tc=g:GetFirst()
-        tc:NegateEffect(e:GetHandler())
+        tc:NegateEffects(e:GetHandler())
     end
     Duel.SpecialSummonComplete()
 end
