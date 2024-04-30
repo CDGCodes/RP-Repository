be5401a3f199760e6e5ddb592961a92b4535b1c6
BionicPlayz <tsanderson2002@gmail.com>
2024-04-30 00:50:14 -0400

    Raiden Eff2

--- script/Bionic's Stuff/c1700000002.lua
+++ script/Bionic's Stuff/c1700000002.lua
@@ -13,6 +13,14 @@ function s.initial_effect(c)
   e1:SetOperation(s.spop)
   e1:SetCost(s.spcost)
   c:RegisterEffect(e1)
+  local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_SEARCH+CATEGORY_TOHAND)
+	e2:SetType(EFFECT_TYPE_TRIGGER_F+EFFECT_TYPE_SINGLE)
+	e2:SetCode(EVENT_LEAVE_FIELD)
+	e2:SetCondition(s.tdcondition)
+	e2:SetTarget(s.tdtarget)
+	e2:SetOperation(s.tdoperation)
+	c:RegisterEffect(e2)
   Duel.AddCustomActivityCounter(id, ACTIVITY_SPSUMMON, s.spfilter)
 end
 function s.spfilter(c)
@@ -35,6 +43,9 @@ end
 function s.spfilter2(c,e,tp)
     return c:IsSetCard(0x38) and c:IsCanBeSpecialSummoned(e, 0, tp, false, false)
 end
+function s.AddFilter(c)
+    return c:IsCode(77558536) and c:IsAbleToHand()
+end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then return Duel.GetLocationCount(tp, LOCATION_MZONE)>0 and Duel.IsExistingMatchingCard(s.spfilter2, tp, LOCATION_HAND|LOCATION_DECK, 0, 1, nil, e, tp) end
     Duel.SetOperationInfo(0, CATEGORY_SPECIAL_SUMMON, nil, 1, tp, LOCATION_HAND|LOCATION_DECK)
@@ -48,4 +59,20 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
         tc:NegateEffects(e:GetHandler())
     end
     Duel.SpecialSummonComplete()
+end
+function s.tdcondition(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsPreviousLocation(LOCATION_MZONE) and c:IsPreviousControler(tp) and rp==1-tp
+end
+function s.tdtarget (e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.tdoperation(e,tp,eg,ep,ev,re,r,rp)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local tc=Duel.GetFirstMatchingCard(s.AddFilter,tp,LOCATION_DECK,0,nil)
+	if tc~=nil then
+		Duel.SendtoHand(tc,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,tc)
+	end
 end
