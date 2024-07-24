6406d042d639bbcdb5c9c0aaa37555f153231421
finsterking <finsterking@gmail.com>
2024-07-24 17:26:48 -0400

    new masked hero

--- script/Hero/c10000955.lua
+++ script/Hero/c10000955.lua
@@ -1,82 +1,82 @@
-local s, id = GetID()
-
+-- Masked HERO Shadow Blade
+local s,id=GetID()
 function s.initial_effect(c)
-    -- Must be Special Summoned with "Mask Change"
-    c:EnableReviveLimit()
-    aux.AddMaskedEff(c, s.mfilter, MASKED_SPELL_EFFECT)
-
-    -- Banish on summon
-    local e1 = Effect.CreateEffect(c)
-    e1:SetDescription(aux.Stringid(id, 0))
-    e1:SetCategory(CATEGORY_REMOVE)
-    e1:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_O)
-    e1:SetCode(EVENT_SPSUMMON_SUCCESS)
-    e1:SetProperty(EFFECT_FLAG_CARD_TARGET + EFFECT_FLAG_DELAY)
-    e1:SetCondition(s.banishcon)
-    e1:SetTarget(s.banishtg)
-    e1:SetOperation(s.banishop)
+    -- Special Summon condition
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+    e1:SetCode(EFFECT_SPSUMMON_CONDITION)
+    e1:SetValue(aux.NOT(aux.TargetBoolFunction(Card.IsCode,21143940))) -- Mask Change card check
     c:RegisterEffect(e1)
 
-    -- Negate attack and banish
-    local e2 = Effect.CreateEffect(c)
-    e2:SetDescription(aux.Stringid(id, 1))
+    -- Banish on Summon
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,0))
     e2:SetCategory(CATEGORY_REMOVE)
-    e2:SetType(EFFECT_TYPE_QUICK_O)
-    e2:SetCode(EVENT_ATTACK_ANNOUNCE)
-    e2:SetRange(LOCATION_MZONE)
-    e2:SetCountLimit(1)
-    e2:SetCondition(s.negatecon)
-    e2:SetOperation(s.negateop)
+    e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+    e2:SetCondition(s.banish_condition)
+    e2:SetTarget(s.banish_target)
+    e2:SetOperation(s.banish_operation)
     c:RegisterEffect(e2)
 
-    -- ATK Boost
-    local e3 = Effect.CreateEffect(c)
-    e3:SetType(EFFECT_TYPE_SINGLE)
-    e3:SetCode(EFFECT_UPDATE_ATTACK)
-    e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    -- Negate Attack
+    local e3=Effect.CreateEffect(c)
+    e3:SetDescription(aux.Stringid(id,1))
+    e3:SetCategory(CATEGORY_NEGATE)
+    e3:SetType(EFFECT_TYPE_QUICK_O)
+    e3:SetCode(EVENT_BE_BATTLE_TARGET)
     e3:SetRange(LOCATION_MZONE)
-    e3:SetValue(s.atkval)
+    e3:SetCountLimit(1,id)
+    e3:SetCondition(s.negate_condition)
+    e3:SetOperation(s.negate_operation)
     c:RegisterEffect(e3)
+
+    -- ATK Boost
+    local e4=Effect.CreateEffect(c)
+    e4:SetType(EFFECT_TYPE_SINGLE)
+    e4:SetCode(EFFECT_UPDATE_ATTACK)
+    e4:SetValue(s.atk_value)
+    c:RegisterEffect(e4)
 end
 
--- Must be Special Summoned with "Mask Change"
-function s.mfilter(c)
-    return c:IsSetCard(0x8) and c:IsLevelAbove(5) and c:IsAttribute(ATTRIBUTE_DARK)
+-- Banish condition
+function s.banish_condition(e,tp,eg,ep,ev,re,r,rp)
+    return e:GetHandler():IsSummonType(SUMMON_TYPE_SPECIAL)
 end
 
--- Banish on summon
-function s.banishcon(e, tp, eg, ep, ev, re, r, rp)
-    return e:GetHandler():IsSummonType(SUMMON_TYPE_SPECIAL + 0x8)
+function s.banish_target(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(nil,tp,0,LOCATION_ONFIELD,1,nil) end
+    Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,1,1-tp,LOCATION_ONFIELD)
 end
 
-function s.banishtg(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
-    if chkc then return chkc:IsOnField() and chkc:IsAbleToRemove() end
-    if chk == 0 then return Duel.IsExistingTarget(Card.IsAbleToRemove, tp, 0, LOCATION_ONFIELD, 1, nil) end
-    Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_REMOVE)
-    local g = Duel.SelectTarget(tp, Card.IsAbleToRemove, tp, 0, LOCATION_ONFIELD, 1, 1, nil)
-    Duel.SetOperationInfo(0, CATEGORY_REMOVE, g, 1, 0, 0)
-end
-
-function s.banishop(e, tp, eg, ep, ev, re, r, rp)
-    local tc = Duel.GetFirstTarget()
-    if tc and tc:IsRelateToEffect(e) then
-        Duel.Remove(tc, POS_FACEUP, REASON_EFFECT)
+function s.banish_operation(e,tp,eg,ep,ev,re,r,rp)
+    local g=Duel.GetMatchingGroup(nil,tp,0,LOCATION_ONFIELD,nil)
+    if #g>0 then
+        local tc=g:Select(tp,1,1,nil):GetFirst()
+        if tc then
+            Duel.Remove(tc,POS_FACEUP,REASON_EFFECT)
+        end
     end
 end
 
--- Negate attack and banish
-function s.negatecon(e, tp, eg, ep, ev, re, r, rp)
-    return Duel.GetAttacker():IsControler(1 - tp)
+-- Negate attack condition
+function s.negate_condition(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    local atk=Duel.GetAttacker()
+    return atk and atk:IsControler(1-tp) and atk:IsFaceup() and atk:IsRelateToBattle()
 end
 
-function s.negateop(e, tp, eg, ep, ev, re, r, rp)
-    local atk = Duel.GetAttacker()
-    if Duel.NegateAttack() then
-        Duel.Remove(atk, POS_FACEUP, REASON_EFFECT)
+function s.negate_operation(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    local atk=Duel.GetAttacker()
+    if atk:IsRelateToBattle() then
+        Duel.NegateAttack()
+        Duel.Remove(atk,POS_FACEUP,REASON_EFFECT)
     end
 end
 
 -- ATK Boost
-function s.atkval(e, c)
-    return Duel.GetMatchingGroupCount(aux.FilterFaceupFunction(Card.IsSetCard, 0x8), c:GetControler(), LOCATION_GRAVE, 0, nil) * 100
+function s.atk_value(e,c)
+    return Duel.GetMatchingGroupCount(Card.IsType,c:GetControler(),LOCATION_GRAVE,0,nil,TYPE_MONSTER)*100
 end
