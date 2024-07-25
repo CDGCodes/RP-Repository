74d8173358c56f1ad71c9d030ea8feb14595f035
Scarecrow <94719017+Kiderobin@users.noreply.github.com>
2024-07-25 03:17:03 -0400

    Update c4000000008.lua

--- script/Volcanic/c4000000008.lua
+++ script/Volcanic/c4000000008.lua
@@ -1,40 +1,89 @@
-local s, id = GetID()
+local s,id=GetID()
 function s.initial_effect(c)
-    -- XYZ summon
-    Xyz.AddProcedure(c, nil, 4, 2)
-    c:EnableReviveLimit()
--- Add on to this effect so during your Main Phase you can attach a Pyro monster from your hand, Grave, or Banish Zone to this card as Xyz material (without any cost)
-function s.xyz_attach_effect(c)
-    local e = Effect.CreateEffect(c)
-    e:SetDescription(aux.Stringid(id,1))
-    e:SetType(EFFECT_TYPE_SINGLE)
-    e:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-    e:SetRange(LOCATION_MZONE)
-    e:SetCode(EFFECT_XYZ_MATERIAL)
-    e:SetCondition(s.xyz_attach_condition)
-    e:SetOperation(s.xyz_attach_operation)
-    c:RegisterEffect(e)
+	--Xyz Summon
+	Xyz.AddProcedure(c,nil,4,2)
+	c:EnableReviveLimit()
+	--Attach a Pyro monster from hand or graveyard as Xyz material
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,1))
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetTarget(s.xyzMaterialTarget)
+	e1:SetOperation(s.xyzMaterialOperation)
+	c:RegisterEffect(e1,false,REGISTER_FLAG_DETACH_XMAT)
+	--Destroy 1 Spell/Trap
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_DESTROY)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetHintTiming(0,TIMING_END_PHASE+TIMING_EQUIP)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1)
+	e2:SetCost(aux.dxmcostgen(1,1,nil))
+	e2:SetTarget(s.target)
+	e2:SetOperation(s.operation)
+	c:RegisterEffect(e2,false,REGISTER_FLAG_DETACH_XMAT)
+	--Inflict damage to opponent based on Xyz materials (end of your turn)
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,2))
+	e3:SetCategory(CATEGORY_DAMAGE)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e3:SetCode(EVENT_PHASE+PHASE_END)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCountLimit(1)
+	e3:SetCondition(s.damageCondition)
+	e3:SetTarget(s.damageTarget)
+	e3:SetOperation(s.damageOperation)
+	c:RegisterEffect(e3)
 end
 
-function s.xyz_attach_condition(e,c,og)
-    if c==nil then return true end
-    local tp=c:GetControler()
-    local mg=Duel.GetMatchingGroup(Card.IsType,tp,LOCATION_HAND+LOCATION_GRAVE+LOCATION_REMOVED,0,nil,TYPE_PYRO)
-    return Duel.GetLocationCount(tp,LOCATION_SZONE)>0 and mg:IsExists(s.xyz_attach_filter,1,nil,tp)
+function s.xyzMaterialFilter(c)
+	return c:IsRace(RACE_PYRO) and (c:IsLocation(LOCATION_HAND) or c:IsLocation(LOCATION_GRAVE))
 end
 
-function s.xyz_attach_filter(c,tp)
-    return c:IsType(TYPE_PYRO) and c:IsControler(tp)
+function s.xyzMaterialTarget(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.xyzMaterialFilter,tp,LOCATION_HAND+LOCATION_GRAVE,0,1,nil) end
 end
 
-function s.xyz_attach_operation(e,tp,eg,ep,ev,re,r,rp,c,og)
-    local mg=Duel.GetMatchingGroup(Card.IsType,tp,LOCATION_HAND+LOCATION_GRAVE+LOCATION_REMOVED,0,nil,TYPE_PYRO)
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_XMATERIAL)
-    local g=mg:FilterSelect(tp,s.xyz_attach_filter,1,1,nil,tp)
-    if g:GetCount()>0 then
-        local tc=g:GetFirst()
-        if not tc:IsImmuneToEffect(e) then
-            Duel.Overlay(c,Group.FromCards(tc))
-        end
-    end
+function s.xyzMaterialOperation(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_XMATERIAL)
+	local g=Duel.SelectMatchingCard(tp,s.xyzMaterialFilter,tp,LOCATION_HAND+LOCATION_GRAVE,0,1,1,nil)
+	if #g>0 then
+		Duel.Overlay(e:GetHandler(),g)
+	end
+end
+
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsOnField() and chkc:IsType(TYPE_SPELL+TYPE_TRAP) end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsType,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil,TYPE_SPELL+TYPE_TRAP) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectTarget(tp,Card.IsType,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil,TYPE_SPELL+TYPE_TRAP)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+end
+
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		Duel.Destroy(tc,REASON_EFFECT)
+	end
+end
+
+function s.damageCondition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetTurnPlayer()==tp
+end
+
+function s.damageTarget(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	local ct=Duel.GetOverlayCount(tp,1,1)
+	Duel.SetTargetPlayer(1-tp)
+	Duel.SetTargetParam(ct*500)
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,ct*500)
+end
+
+function s.damageOperation(e,tp,eg,ep,ev,re,r,rp)
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.Damage(p,d,REASON_EFFECT)
 end
