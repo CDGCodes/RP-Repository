46400b9b6a505c71ccdb6c897df4b9fe870412d9
BionicPlayz <tsanderson2002@gmail.com>
2024-08-04 16:22:32 -0400

    Add Nier Summon

---
+++ script/Bionic's Stuff/c1700000004.lua
@@ -0,0 +1,24 @@
+-- Chaos Basilisk Nier
+
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	Pendulum.AddProcedure(c)
+    --Must be special summoned by card effect
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
+	e1:SetValue(s.splimit)
+	c:RegisterEffect(e1)
+	--Special summon itself from hand
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_HAND)
+	e2:SetCountLimit(1,id)
+	e2:SetCost(s.spcost)
+	e2:SetTarget(s.sptg)
+	e2:SetOperation(s.spop)
+	c:RegisterEffect(e2)
