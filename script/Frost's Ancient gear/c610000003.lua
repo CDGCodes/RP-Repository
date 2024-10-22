8e71e76aae10ddcb5bf96d00ba95bdc0276f0af1
unknown <roadkill711@gmail.com>
2024-10-22 02:31:56 +0100

    Double hunting hound

--- script/Frost's Ancient gear/c610000003.lua
+++ script/Frost's Ancient gear/c610000003.lua
@@ -44,11 +44,23 @@ function s.initial_effect(c)
 	e5:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
 	e5:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	e5:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e5:SetCountLimit(1,0)
+	e5:SetCountLimit(1,{id,1})
 	e5:SetTarget(s.damtg)
 	e5:SetOperation(s.damop)
 	e5:SetCondition(s.damcon)
 	c:RegisterEffect(e5)
+	
+    --Activate
+    local e6=Effect.CreateEffect(c)
+    e6:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e6:SetType(EFFECT_TYPE_IGNITION)
+    e6:SetRange(LOCATION_MZONE)
+    e6:SetCode(EVENT_FREE_CHAIN)
+	e6:SetCountLimit(1,{id,2})
+    e6:SetCost(s.cost)
+    e6:SetTarget(s.target)
+    e6:SetOperation(s.activate)
+    c:RegisterEffect(e6)
 
 end
 s.material_setcode=0x7
@@ -122,3 +134,40 @@ function s.damop(e,tp,eg,ep,ev,re,r,rp)
 	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
 	Duel.Damage(p,d,REASON_EFFECT)
 end
+
+function s.cost(e, tp, eg, ep, ev, re, r, rp, chk)
+    local c=e:GetHandler()
+    if chk==0 then return c:IsAbleToExtra() end
+    Duel.SendtoDeck(c, nil, 0, REASON_COST)
+end
+function s.mgfilter(c,e,tp,fusc,mg)
+    return c:IsControler(tp) and c:IsLocation(LOCATION_GRAVE)
+        and (c:GetReason()&0x40008)==0x40008 and c:GetReasonCard()==fusc
+        and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+        and fusc:CheckFusionMaterial(mg,c,PLAYER_NONE+65536)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+    local g=e:GetHandler():GetMaterial()
+    if chk==0 then
+        local ct=#g
+        local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+        if e:GetHandler():GetSequence()<5 then ft=ft+1 end
+        return ct>0 and ft>=ct and not Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT)
+            and e:GetHandler():IsSummonType(SUMMON_TYPE_FUSION)
+            and g:FilterCount(s.mgfilter,nil,e,tp,e:GetHandler(),g)==ct
+    end
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,#g,0,0)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+    if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then return end
+    local g=e:GetHandler():GetMaterial()
+    local ct=#g
+    if Duel.GetLocationCount(tp,LOCATION_MZONE)>=ct
+        and g:FilterCount(s.mgfilter,nil,e,tp,e:GetHandler(),g)==ct then
+        Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+    end
+end
+
+ 
+ 
+ 
--- script/Frost's Ancient gear/c610000008.lua
+++ script/Frost's Ancient gear/c610000008.lua
@@ -1,4 +1,4 @@
---歯車街
+--Gear section
 local s,id=GetID()
 function s.initial_effect(c)
 	--activate
