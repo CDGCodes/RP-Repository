4bd96192c18c795df70d8f1f07478c8612ae8ea3
Scarecrow <94719017+Kiderobin@users.noreply.github.com>
2024-12-04 21:53:25 -0500

    Add files via upload

---
+++ script/c1000000032.lua
@@ -0,0 +1,60 @@
+--Number 109: Black Luster Soldier - Cosmic Soldier
+local s,id=GetID()
+function s.initial_effect(c)
+  --xyz summon
+  Xyz.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsLevel,8),2,nil,nil,99)
+  c:EnableReviveLimit()
+  
+  --Once per turn: Banish opponent's card
+  local e1=Effect.CreateEffect(c)
+  e1:SetDescription(aux.Stringid(id,0))
+  e1:SetCategory(CATEGORY_REMOVE)
+  e1:SetType(EFFECT_TYPE_IGNITION)
+  e1:SetRange(LOCATION_MZONE)
+  e1:SetCountLimit(1)
+  e1:SetCost(s.cost)
+  e1:SetTarget(s.target)
+  e1:SetOperation(s.operation)
+  c:RegisterEffect(e1,false,REGISTER_FLAG_DETACH_XMAT)
+  
+  --Additional effects if used "Black Luster Soldier" monster as material
+  local e2=Effect.CreateEffect(c)
+  e2:SetType(EFFECT_TYPE_SINGLE)
+  e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+  e2:SetRange(LOCATION_MZONE)
+  e2:SetCondition(s.matcon)
+  e2:SetValue(1)
+  e2:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+  e2:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+  c:RegisterEffect(e2)
+  
+  local e3=Effect.CreateEffect(c)
+  e3:SetType(EFFECT_TYPE_SINGLE)
+  e3:SetCode(EFFECT_PIERCE)
+  e3:SetCondition(s.matcon)
+  c:RegisterEffect(e3)
+end
+
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+  if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST) end
+  e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_COST)
+end
+
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+  if chkc then return chkc:IsOnField() and chkc:IsControler(1-tp) end
+  if chk==0 then return Duel.IsExistingTarget(aux.TRUE,tp,0,LOCATION_ONFIELD,1,nil) end
+  Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+  local g=Duel.SelectTarget(tp,aux.TRUE,tp,0,LOCATION_ONFIELD,1,1,nil)
+  Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,1,0,0)
+end
+
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+  local tc=Duel.GetFirstTarget()
+  if tc and tc:IsRelateToEffect(e) then
+    Duel.Remove(tc,POS_FACEUP,REASON_EFFECT)
+  end
+end
+
+function s.matcon(e)
+  return e:GetHandler():IsSetCard(0x10cf)
+end
