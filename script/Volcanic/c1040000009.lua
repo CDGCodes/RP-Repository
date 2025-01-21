b540de4eb6ee06c1286768af8cbe5e9b9969a1cb
Scarecrow <94719017+Kiderobin@users.noreply.github.com>
2025-01-20 20:44:27 -0500

    Update c1040000009.lua

--- script/Volcanic/c1040000009.lua
+++ script/Volcanic/c1040000009.lua
@@ -3,12 +3,12 @@ local s, id = GetID()
 function s.initial_effect(c)
     -- Special Summon Condition
     c:EnableReviveLimit()
-    aux.AddFusionProcMix(c, true, true, aux.FilterBoolFunction(Card.IsSetCard,0x32), aux.FilterBoolFunction(Card.IsSetCard,0x32), aux.FilterBoolFunction(Card.IsCode,CARD_BLAZE_ACCELERATOR))
-    Fusion.AddContactProc(c,s.contactfil,s.contactop,true)
+    Fusion.AddProcMix(c, true, true, s.ffilter1, s.ffilter1, s.ffilter2)
+    Fusion.AddContactProc(c, s.contactfil, s.contactop, true)
 
     -- Inflict Damage
-    local e1=Effect.CreateEffect(c)
-    e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+    local e1 = Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_FIELD + EFFECT_TYPE_CONTINUOUS)
     e1:SetCode(EVENT_TO_GRAVE)
     e1:SetRange(LOCATION_MZONE)
     e1:SetCondition(s.damcon)
@@ -16,7 +16,7 @@ function s.initial_effect(c)
     c:RegisterEffect(e1)
 
     -- Increase ATK and DEF
-    local e2=Effect.CreateEffect(c)
+    local e2 = Effect.CreateEffect(c)
     e2:SetDescription(aux.Stringid(id, 0))
     e2:SetCategory(CATEGORY_ATKCHANGE + CATEGORY_DEFCHANGE)
     e2:SetType(EFFECT_TYPE_IGNITION)
@@ -27,7 +27,7 @@ function s.initial_effect(c)
     c:RegisterEffect(e2)
 
     -- Return to Extra Deck
-    local e3=Effect.CreateEffect(c)
+    local e3 = Effect.CreateEffect(c)
     e3:SetType(EFFECT_TYPE_IGNITION)
     e3:SetRange(LOCATION_GRAVE)
     e3:SetCost(s.retcost)
@@ -38,44 +38,60 @@ end
 function s.ffilter1(c)
     return c:IsSetCard(0x32) and c:IsType(TYPE_MONSTER)
 end
+
 function s.ffilter2(c)
-    return c:IsCode(CARD_BLAZE_ACCELERATOR)
+    return c:IsSetCard(0xb9) and c:IsType(TYPE_SPELL + TYPE_TRAP)
 end
 
-function s.damcon(e,tp,eg,ep,ev,re,r,rp)
-    return eg:IsExists(Card.IsAttribute,1,nil,ATTRIBUTE_FIRE)
-end
-function s.damop(e,tp,eg,ep,ev,re,r,rp)
-    Duel.Damage(1-tp, 500, REASON_EFFECT)
+function s.contactfil(tp)
+    return Duel.GetMatchingGroup(s.ffilter1, tp, LOCATION_ONFIELD, 0, nil)
+        + Duel.GetMatchingGroup(s.ffilter2, tp, LOCATION_ONFIELD, 0, nil)
 end
 
-function s.atkcost(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,2,nil) end
-    Duel.DiscardHand(tp,Card.IsDiscardable,2,2,REASON_COST+REASON_DISCARD)
+function s.contactop(g)
+    Duel.SendtoDeck(g, nil, SEQ_DECKSHUFFLE, REASON_COST + REASON_MATERIAL + REASON_FUSION)
 end
-function s.atkop(e,tp,eg,ep,ev,re,r,rp)
-    local c=e:GetHandler()
+
+function s.damcon(e, tp, eg, ep, ev, re, r, rp)
+    return eg:IsExists(Card.IsControler, 1, nil, tp) and eg:IsExists(Card.IsAttribute, 1, nil, ATTRIBUTE_FIRE)
+end
+
+function s.damop(e, tp, eg, ep, ev, re, r, rp)
+    local ct = eg:FilterCount(Card.IsControler, nil, tp)
+    ct = ct * eg:FilterCount(Card.IsAttribute, nil, ATTRIBUTE_FIRE)
+    Duel.Damage(1 - tp, ct * 500, REASON_EFFECT)
+end
+
+function s.atkcost(e, tp, eg, ep, ev, re, r, rp, chk)
+    if chk == 0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable, tp, LOCATION_HAND, 0, 2, nil) end
+    Duel.DiscardHand(tp, Card.IsDiscardable, 2, 2, REASON_COST + REASON_DISCARD)
+end
+
+function s.atkop(e, tp, eg, ep, ev, re, r, rp)
+    local c = e:GetHandler()
     if c:IsRelateToEffect(e) and c:IsFaceup() then
-        local e1=Effect.CreateEffect(c)
+        local e1 = Effect.CreateEffect(c)
         e1:SetType(EFFECT_TYPE_SINGLE)
         e1:SetCode(EFFECT_UPDATE_ATTACK)
         e1:SetValue(1000)
-        e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+        e1:SetReset(RESET_EVENT + RESETS_STANDARD + RESET_PHASE + PHASE_END)
         c:RegisterEffect(e1)
-        local e2=e1:Clone()
+        local e2 = e1:Clone()
         e2:SetCode(EFFECT_UPDATE_DEFENSE)
         c:RegisterEffect(e2)
     end
 end
 
-function s.retcost(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.IsExistingMatchingCard(s.retfilter,tp,LOCATION_GRAVE+LOCATION_MZONE,0,3,nil) end
-    local g=Duel.SelectMatchingCard(tp,s.retfilter,tp,LOCATION_GRAVE+LOCATION_MZONE,0,3,3,nil)
-    Duel.Remove(g,POS_FACEUP,REASON_COST)
+function s.retcost(e, tp, eg, ep, ev, re, r, rp, chk)
+    if chk == 0 then return Duel.IsExistingMatchingCard(s.retfilter, tp, LOCATION_GRAVE + LOCATION_MZONE, 0, 3, nil) end
+    local g = Duel.SelectMatchingCard(tp, s.retfilter, tp, LOCATION_GRAVE + LOCATION_MZONE, 0, 3, 3, nil)
+    Duel.Remove(g, POS_FACEUP, REASON_COST)
 end
+
 function s.retfilter(c)
     return c:IsType(TYPE_MONSTER) and c:IsRace(RACE_PYRO)
 end
-function s.retop(e,tp,eg,ep,ev,re,r,rp)
-    Duel.SendtoDeck(e:GetHandler(),nil,SEQ_DECKSHUFFLE,REASON_EFFECT)
+
+function s.retop(e, tp, eg, ep, ev, re, r, rp)
+    Duel.SendtoDeck(e:GetHandler(), nil, SEQ_DECKSHUFFLE, REASON_EFFECT)
 end
