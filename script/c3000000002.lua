c1e0e0c6899d5e1f15f99525a724d3bd99544997
Scarecrow <94719017+Kiderobin@users.noreply.github.com>
2025-02-27 01:26:01 -0500

    Update c3000000002.lua

--- script/c3000000002.lua
+++ script/c3000000002.lua
@@ -22,6 +22,7 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
     local g=Duel.GetMatchingGroup(Card.IsType,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_MZONE,0,nil,TYPE_MONSTER)
     local tc=g:GetFirst()
     while tc do
+        -- Apply the attribute change to monsters in hand, deck, and on the field
         local e1=Effect.CreateEffect(c)
         e1:SetType(EFFECT_TYPE_SINGLE)
         e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
@@ -31,4 +32,18 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
         tc:RegisterEffect(e1)
         tc=g:GetNext()
     end
+    
+    -- Retain attribute when moved to hand, Deck, or Graveyard
+    local e2=Effect.CreateEffect(c)
+    e2:SetType(EFFECT_TYPE_FIELD)
+    e2:SetCode(EFFECT_CHANGE_ATTRIBUTE)
+    e2:SetTargetRange(LOCATION_HAND+LOCATION_DECK+LOCATION_MZONE+LOCATION_GRAVE,0)
+    e2:SetTarget(s.atttg)
+    e2:SetValue(attribute)
+    e2:SetReset(RESET_PHASE+PHASE_END)
+    Duel.RegisterEffect(e2,tp)
+end
+
+function s.atttg(e,c)
+    return c:IsType(TYPE_MONSTER)
 end
