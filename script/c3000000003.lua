922970085665b27312fd45dfd461e899e5f76292
Scarecrow <94719017+Kiderobin@users.noreply.github.com>
2025-02-28 09:58:44 -0500

    Update c3000000003.lua

--- script/c3000000003.lua
+++ script/c3000000003.lua
@@ -1,107 +1,50 @@
---Corrupted wind
+--The Corrupted Wind
 local s,id=GetID()
-
 function s.initial_effect(c)
-    --special summon condition
+    --Special summon condition
     local e1=Effect.CreateEffect(c)
     e1:SetType(EFFECT_TYPE_FIELD)
     e1:SetCode(EFFECT_SPSUMMON_PROC)
     e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
     e1:SetRange(LOCATION_HAND)
     e1:SetCondition(s.spcon)
+    e1:SetOperation(s.spop)
     c:RegisterEffect(e1)
     
-    --special summon wind attribute monster
-    local e2=Effect.CreateEffect(c)
-    e2:SetDescription(aux.Stringid(id,0))
-    e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
-    e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-    e2:SetCode(EVENT_PHASE+PHASE_END)
-    e2:SetRange(LOCATION_MZONE)
-    e2:SetCountLimit(1)
-    e2:SetCost(s.spcost)
-    e2:SetTarget(s.sptg)
-    e2:SetOperation(s.spop)
-    c:RegisterEffect(e2)
-
-    --quick effect to set WIND monster
-    local e3=Effect.CreateEffect(c)
-    e3:SetDescription(aux.Stringid(id,1))
-    e3:SetCategory(CATEGORY_TOGRAVE)
-    e3:SetType(EFFECT_TYPE_QUICK_O)
-    e3:SetCode(EVENT_FREE_CHAIN)
-    e3:SetRange(LOCATION_MZONE)
-    e3:SetCountLimit(1,id)
-    e3:SetTarget(s.settg)
-    e3:SetOperation(s.setop)
-    c:RegisterEffect(e3)
+    --Track activation of the card with ID 3000000002 and WIND attribute choice
+    if not s.global_check then
+        s.global_check=true
+        local ge1=Effect.CreateEffect(c)
+        ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+        ge1:SetCode(EVENT_CHAINING)
+        ge1:SetOperation(s.checkop)
+        Duel.RegisterEffect(ge1,0)
+    end
 end
 
---special summon condition function
+function s.checkop(e,tp,eg,ep,ev,re,r,rp)
+    if re:GetHandler():IsCode(3000000002) and re:IsHasType(EFFECT_TYPE_ACTIVATE) then
+        Duel.RegisterFlagEffect(ep,3000000002,RESET_PHASE+PHASE_END,0,1,ATTRIBUTE_WIND)
+    end
+end
+
+--Special summon condition function
 function s.spcon(e,c)
     if c==nil then return true end
-    return Duel.IsExistingMatchingCard(s.spfilter,c:GetControler(),LOCATION_GRAVE,0,1,nil)
+    local tp=e:GetHandlerPlayer()
+    return Duel.GetFlagEffect(tp,3000000002)>0
+        and Duel.GetFlagEffectLabel(tp,3000000002)==ATTRIBUTE_WIND
+        and Duel.IsExistingMatchingCard(s.spcostfilter,tp,LOCATION_HAND,0,2,nil)
 end
 
---filter to check the specific card ID
-function s.spfilter(c)
-    return c:IsCode(3000000002)
+--Special summon operation function
+function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+    local g=Duel.SelectMatchingCard(tp,s.spcostfilter,tp,LOCATION_HAND,0,2,2,nil)
+    Duel.Destroy(g,REASON_COST)
 end
 
---special summon cost function
-function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.IsExistingMatchingCard(s.spfilter2,tp,LOCATION_GRAVE,0,1,nil,e,tp)
-        and Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,nil) end
-    Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_COST+REASON_DISCARD)
-end
-
---special summon target function
-function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-        and Duel.IsExistingMatchingCard(s.spfilter2,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
-    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
-end
-
---special summon operation function
-function s.spop(e,tp,eg,ep,ev,re,r,rp)
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-    local g=Duel.SelectMatchingCard(tp,s.spfilter2,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
-    if #g>0 then
-        Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
-    end
-end
-
---filter to check WIND attribute monsters
-function s.spfilter2(c,e,tp)
-    return c:IsAttribute(ATTRIBUTE_WIND) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
-end
-
---set WIND monster target function
-function s.settg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.IsExistingMatchingCard(s.setfilter,tp,LOCATION_MZONE,0,1,nil) end
-    Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_MZONE)
-end
-
---set WIND monster operation function
-function s.setop(e,tp,eg,ep,ev,re,r,rp)
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOZONE)
-    local g=Duel.SelectMatchingCard(tp,s.setfilter,tp,LOCATION_MZONE,0,1,1,nil)
-    if #g>0 then
-        local tc=g:GetFirst()
-        if Duel.MoveToField(tc,tp,tp,LOCATION_SZONE,POS_FACEUP,true) then
-            --set as Continuous Trap Card
-            local e1=Effect.CreateEffect(e:GetHandler())
-            e1:SetCode(EFFECT_CHANGE_TYPE)
-            e1:SetType(EFFECT_TYPE_SINGLE)
-            e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-            e1:SetReset(RESET_EVENT+RESETS_STANDARD-RESET_TURN_SET)
-            e1:SetValue(TYPE_TRAP+TYPE_CONTINUOUS)
-            tc:RegisterEffect(e1)
-        end
-    end
-end
-
---filter to check WIND attribute monsters to set
-function s.setfilter(c)
-    return c:IsAttribute(ATTRIBUTE_WIND) and c:IsAbleToChangeControler()
+--Filter to check WIND attribute monsters, excluding this card
+function s.spcostfilter(c)
+    return c:IsAttribute(ATTRIBUTE_WIND) and c:IsDestructable() and not c:IsCode(3000000003)
 end
