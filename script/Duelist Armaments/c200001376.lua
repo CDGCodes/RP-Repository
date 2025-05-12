4adc925bea257f4eafcdf05c7e4992bd91093b02
CDGCodes <dpunter8562@gmail.com>
2025-05-12 03:08:21 -0700

    Changed effect text of Iridescent Steed

--- script/Duelist Armaments/c200001376.lua
+++ script/Duelist Armaments/c200001376.lua
@@ -8,7 +8,6 @@ function s.initial_effect(c)
     Nexus.AddProcedure(c, nil, true, 2, 99)
     -- Equip from grave
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
@@ -19,6 +18,8 @@ function s.initial_effect(c)
 	e1:SetOperation(s.eqop)
 	c:RegisterEffect(e1)
     -- Swap equips on field
+    local e2=Effect.CreateEffect(c)
+    e2:
     -- Inherit effect
 end
 
