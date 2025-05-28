ffa9e99b206ac3fa18364df64053a2cc8cbe95f5
Kronk-Meister <landon.m.ford@icloud.com>
2025-05-28 13:49:54 -0500

    monarch of the lair effect

--- script/c100006348.lua
+++ script/c100006348.lua
@@ -37,6 +37,16 @@ local e5=Effect.CreateEffect(c)
 	e5:SetCode(EFFECT_UPDATE_ATTACK)
 	e5:SetValue(s.value)
 	c:RegisterEffect(e5)
+    local e6=Effect.CreateEffect(c)
+	e6:SetCategory(CATEGORY_DESTROY)
+	e6:SetType(EFFECT_TYPE_IGNITION)
+	e6:SetCode(EVENT_FREE_CHAIN)
+	e6:SetCost(s.cost)
+	e6:SetTarget(s.target)
+	e6:SetOperation(s.activate)
+    e6:SetRange(LOCATION_MZONE)
+    e6:SetCountLimit(2,id,EFFECT_COUNT_CODE_DUEL)
+	c:RegisterEffect(e6)
 end
 function s.sumlimit(e,c)
 	if not c then return false end
@@ -50,4 +60,99 @@ function s.atkfilter(c)
 end
 function s.value(e,c)
 	return Duel.GetMatchingGroupCount(s.atkfilter,c:GetControler(),LOCATION_GRAVE,0,nil)*500
+end
+function s.costfilter(c)
+	return c:IsAttribute(ATTRIBUTE_DARK)
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckReleaseGroupCost(tp,s.costfilter,1,false,nil,nil) end
+	local g=Duel.SelectReleaseGroupCost(tp,s.costfilter,1,1,false,nil,nil)
+	Duel.Release(g,REASON_COST)
+end
+function s.tgfilter(c,ty)
+	return c:IsFaceup() and c:IsType(ty)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,2))
+	local ac=Duel.SelectOption(tp,aux.Stringid(id,0),aux.Stringid(id,1))
+	local ty=TYPE_SPELL
+	if ac==1 then ty=TYPE_TRAP end
+	e:SetLabel(ty)
+	local g=Duel.GetMatchingGroup(s.tgfilter,tp,0,LOCATION_ONFIELD,nil,ty)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,#g,0,0)
+end
+function s.cffilter(c)
+	return c:IsLocation(LOCATION_HAND) or (c:IsFacedown() and c:IsSpellTrap())
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local ty=e:GetLabel()
+	local g=Duel.GetFieldGroup(tp,0,LOCATION_ONFIELD|LOCATION_HAND)
+	if #g>0 then
+		local cg=g:Filter(s.cffilter,nil)
+		Duel.ConfirmCards(tp,cg)
+		local dg=g:Filter(Card.IsType,nil,ty)
+		Duel.Destroy(dg,REASON_EFFECT)
+		Duel.ShuffleHand(1-tp)
+	end
+	if not e:IsHasType(EFFECT_TYPE_ACTIVATE) then return end
+	local c=e:GetHandler()
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCode(EVENT_DRAW)
+	e1:SetOperation(s.desop)
+	e1:SetLabel(ty)
+	e1:SetReset(RESET_PHASE|PHASE_END|RESET_OPPO_TURN,3)
+	Duel.RegisterEffect(e1,tp)
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e2:SetCode(EVENT_PHASE+PHASE_END)
+	e2:SetCountLimit(1)
+	e2:SetCondition(s.turncon)
+	e2:SetOperation(s.turnop)
+	e2:SetReset(RESET_PHASE|PHASE_END|RESET_OPPO_TURN,3)
+	Duel.RegisterEffect(e2,tp)
+	e2:SetLabelObject(e1)
+	local descnum=tp==c:GetOwner() and 0 or 1
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetDescription(aux.Stringid(id,descnum))
+	e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_IGNORE_IMMUNE+EFFECT_FLAG_SET_AVAILABLE)
+	e3:SetCode(1082946)
+	e3:SetLabelObject(e2)
+	e3:SetOwnerPlayer(tp)
+	e3:SetOperation(s.reset)
+	e3:SetReset(RESET_PHASE|PHASE_END|RESET_OPPO_TURN,3)
+	c:RegisterEffect(e3)
+	local e4=Effect.CreateEffect(e:GetHandler())
+	e4:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
+	e4:SetDescription(aux.Stringid(id,3))
+	e4:SetReset(RESET_PHASE|PHASE_END|RESET_OPPO_TURN,3)
+	e4:SetTargetRange(0,1)
+	Duel.RegisterEffect(e4,tp)
+end
+function s.reset(e,tp,eg,ep,ev,re,r,rp)
+	s.turnop(e:GetLabelObject(),tp,eg,ep,ev,e,r,rp)
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	if ep==e:GetOwnerPlayer() then return end
+	local hg=eg:Filter(Card.IsLocation,nil,LOCATION_HAND)
+	if #hg==0 then return end
+	Duel.ConfirmCards(1-ep,hg)
+	local dg=hg:Filter(Card.IsType,nil,e:GetLabel())
+	Duel.Destroy(dg,REASON_EFFECT)
+	Duel.ShuffleHand(ep)
+end
+function s.turncon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsTurnPlayer(1-tp)
+end
+function s.turnop(e,tp,eg,ep,ev,re,r,rp)
+	local ct=e:GetLabel()+1
+	e:SetLabel(ct)
+	e:GetHandler():SetTurnCounter(ct)
+	if ct==3 then
+		e:GetLabelObject():Reset()
+		if re then re:Reset() end
+	end
 end
