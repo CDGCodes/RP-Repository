b2ef22666d4f056185631bae63eb09094486caab
Kronk-Meister <landon.m.ford@icloud.com>
2025-05-28 13:28:01 -0500

    added effect for Darkest Diabolos, Monarch of the Lair

---
+++ script/c100006348.lua
@@ -0,0 +1,53 @@
+--Darkest Diabolos, Monarch of the Lair
+local s,id=GetID()
+function s.initial_effect(c)
+    Synchro.AddProcedure(c, nil, 1, 1, Synchro.NonTuner(nil), 1, 1)
+    c:EnableReviveLimit()
+    --INDESTRUCABLE
+    local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+	e1:SetValue(1)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+	c:RegisterEffect(e2)
+    local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCode(EFFECT_UNRELEASABLE_SUM)
+	e3:SetValue(s.sumlimit)
+	c:RegisterEffect(e3)
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_FIELD)
+	e4:SetCode(EFFECT_CANNOT_RELEASE)
+	e4:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetTargetRange(0,1)
+	e4:SetTarget(s.relval)
+	e4:SetValue(1)
+	c:RegisterEffect(e4)
+local e5=Effect.CreateEffect(c)
+	e5:SetType(EFFECT_TYPE_SINGLE)
+	e5:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e5:SetRange(LOCATION_MZONE)
+	e5:SetCode(EFFECT_UPDATE_ATTACK)
+	e5:SetValue(s.value)
+	c:RegisterEffect(e5)
+end
+function s.sumlimit(e,c)
+	if not c then return false end
+	return not c:IsControler(e:GetHandlerPlayer())
+end
+function s.relval(e,c)
+	return c==e:GetHandler()
+end
+function s.atkfilter(c)
+    return c:IsMonster() and c:IsAttribute(ATTRIBUTE_DARK)
+end
+function s.value(e,c)
+	return Duel.GetMatchingGroupCount(s.atkfilter,c:GetControler(),LOCATION_GRAVE,0,nil)*500
+end
