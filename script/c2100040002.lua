828af534ec368b8138c027eb880bed5a8161d01b
Scarecrow <94719017+Kiderobin@users.noreply.github.com>
2025-12-30 21:35:47 -0500

    Update c2100040002.lua

--- script/c2100040002.lua
+++ script/c2100040002.lua
@@ -31,7 +31,7 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
     local c=e:GetHandler()
     local attribute=Duel.AnnounceAttribute(tp,1,ATTRIBUTE_ALL)
     -- Change attribute for existing monsters in hand, Deck, and field
-    local g=Duel.GetMatchingGroup(Card.IsMonsterCard,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_ONFIELD,0,nil)
+    local g=Duel.GetMatchingGroup(Card.IsMonsterCard,tp,LOCATION_HAND+LOCATION_ONFIELD,0,nil)
     local tc=g:GetFirst()
     while tc do
         local e1=Effect.CreateEffect(c)
@@ -56,7 +56,7 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 end
 
 function s.adjustop(e,tp,attribute)
-    local g=Duel.GetMatchingGroup(Card.IsMonsterCard,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_ONFIELD,0,nil)
+    local g=Duel.GetMatchingGroup(Card.IsMonsterCard,tp,LOCATION_HAND+LOCATION_ONFIELD,0,nil)
     local tc=g:GetFirst()
     while tc do
         if tc:GetAttribute()~=attribute then
