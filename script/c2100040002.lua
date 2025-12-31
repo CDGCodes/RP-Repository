26c69412d6bd8ceb5e291a7e8ac893ff880c9097
Scarecrow <94719017+Kiderobin@users.noreply.github.com>
2025-12-30 23:29:38 -0500

    Enhance attribute change effect with search functionality
    Added functionality to search for a Monster from the Deck that matches the declared Attribute after applying effects.

--- script/c2100040002.lua
+++ script/c2100040002.lua
@@ -9,18 +9,6 @@ function s.initial_effect(c)
     e1:SetOperation(s.activate)
     c:RegisterEffect(e1)
 
-    -- Add: Give up your normal draw to search 1 Monster (set 0x2005)
-    local e2=Effect.CreateEffect(c)
-    e2:SetDescription(aux.Stringid(id,1))
-    e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
-    e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-    e2:SetCode(EVENT_PREDRAW)
-    e2:SetRange(LOCATION_HAND)
-    e2:SetCondition(s.thcon)
-    e2:SetCost(s.thcost)
-    e2:SetTarget(s.thtg)
-    e2:SetOperation(s.thop)
-    c:RegisterEffect(e2)
 end
 
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
@@ -53,6 +41,19 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
     end)
     Duel.RegisterEffect(e2,tp)
     Duel.RegisterFlagEffect(1,id,0,0,0,attribute)
+
+    -- After declaring Attribute: add 1 Monster from Deck that lists this card's ID and matches the declared Attribute to hand
+    local function deck_filter(card)
+        return card:IsMonster() and Card.ListsCode(card,id) and card:IsAttribute(attribute) and card:IsAbleToHand()
+    end
+    if Duel.IsExistingMatchingCard(deck_filter, tp, LOCATION_DECK, 0, 1, nil) then
+        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+        local sg=Duel.SelectMatchingCard(tp, deck_filter, tp, LOCATION_DECK, 0, 1, 1, nil)
+        if #sg>0 then
+            Duel.SendtoHand(sg,nil,REASON_EFFECT)
+            Duel.ConfirmCards(1-tp,sg)
+        end
+    end
 end
 
 function s.adjustop(e,tp,attribute)
@@ -70,42 +71,4 @@ function s.adjustop(e,tp,attribute)
         end
         tc=g:GetNext()
     end
-end
-
--- Add: search helpers (searches deck for a Monster with setcode 0x2005)
-function s.thcon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.IsTurnPlayer(tp) and Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)>0
-		and Duel.GetDrawCount(tp)>0 and (Duel.GetTurnCount()>1 or Duel.IsDuelType(DUEL_1ST_TURN_DRAW))
-end
-function s.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return not e:GetHandler():IsPublic() end
-end
-function s.thfilter(c)
-	return c:IsSetCard(0x2005) and c:IsMonster() and c:IsAbleToHand()
-end
-function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
-end
-function s.thop(e,tp,eg,ep,ev,re,r,rp)
-	local dt=Duel.GetDrawCount(tp)
-	if dt==0 then return false end
-	_replace_count=1
-	_replace_max=dt
-	--Give up your normal draw this turn
-	local e1=Effect.CreateEffect(e:GetHandler())
-	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-	e1:SetCode(EFFECT_DRAW_COUNT)
-	e1:SetTargetRange(1,0)
-	e1:SetReset(RESET_PHASE|PHASE_DRAW)
-	e1:SetValue(0)
-	Duel.RegisterEffect(e1,tp)
-	if _replace_count>_replace_max then return end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
-	if #g>0 then
-		Duel.SendtoHand(g,nil,REASON_EFFECT)
-		Duel.ConfirmCards(1-tp,g)
-	end
 end
